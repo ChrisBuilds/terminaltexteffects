8da633d8c5a2892aa44ff905c9b322ac63a3e237
Chris <741258@pm.me>
2023-07-29 14:52:09 -0500

    removes useless stuff

--- tte.py
+++ tte.py
@@ -1,16 +1,11 @@
 import utils.terminaloperations as tops
 from effects import expand, pour, random_sequence, scattered, sparkler, rain
-import random
 
-""" import sys
 
-sys.stdin = open("/dev/tty")
-breakpoint() """
-
-test_data = """This
+test_data = """!This
     is
         test
-            data.
+            data.@
     """
 
 
@@ -19,12 +14,12 @@ def main():
     if not input_data:
         input_data = "No input data."
     # input_data = test_data
-    # tte_effect = pour.PouringEffect(input_data, pour.PourDirection.DOWN)
+    tte_effect = pour.PouringEffect(input_data, pour.PourDirection.DOWN)
     # tte_effect = scattered.ScatteredEffect(input_data)
     # tte_effect = expand.ExpandEffect(input_data)
     # tte_effect = random_sequence.RandomSequence(input_data)
-    # tte_effect = sparkler.SparklerEffect(input_data, sparkler.SparklerPosition.E)
-    tte_effect = rain.RainEffect(input_data)
+    # tte_effect = sparkler.SparklerEffect(input_data, sparkler.SparklerPosition.SE)
+    # tte_effect = rain.RainEffect(input_data)
     tte_effect.run(rate=0.01)
 
 
