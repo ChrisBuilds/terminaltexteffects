44e57e2752133175dc9d8f9828658dbdbd9c658e
Chris <741258@pm.me>
2023-07-30 10:30:26 -0500

    updates to new EffectCharacter interface

--- effects/scattered.py
+++ effects/scattered.py
@@ -1,10 +1,10 @@
 import time
 import utils.terminaloperations as tops
-from effects import base_effect
+from effects import effect
 import random
 
 
-class ScatteredEffect(base_effect.Effect):
+class ScatteredEffect(effect.Effect):
     """Effect that draws the characters into position from random starting locations."""
 
     def __init__(self, input_data: str):
@@ -13,13 +13,9 @@ class ScatteredEffect(base_effect.Effect):
     def prepare_data(self) -> None:
         """Prepares the data for the effect by scattering the characters within range of the input width and height."""
         for character in self.characters:
-            self.animating_chars.append(
-                base_effect.EffectCharacter(
-                    character=character,
-                    current_x=random.randint(1, self.input_width - 1),
-                    current_y=random.randint(1, self.input_height - 1),
-                )
-            )
+            character.current_coord.column = random.randint(1, self.input_width - 1)
+            character.current_coord.row = random.randint(1, self.input_height - 1)
+            self.animating_chars.append(character)
 
     def run(self, rate: float = 0) -> None:
         """Runs the effect.
@@ -32,26 +28,15 @@ class ScatteredEffect(base_effect.Effect):
         while self.pending_chars or self.animating_chars:
             self.animate_chars(rate)
             self.completed_chars.extend(
-                [
-                    completed_char
-                    for completed_char in self.animating_chars
-                    if completed_char.last_x == completed_char.target_x
-                    and completed_char.last_y == completed_char.target_y
-                ]
+                [completed_char for completed_char in self.animating_chars if completed_char.animation_completed()]
             )
             self.maintain_completed()
             self.animating_chars = [
-                animating_char
-                for animating_char in self.animating_chars
-                if animating_char.last_x != animating_char.target_x or animating_char.last_y != animating_char.target_y
+                animating_char for animating_char in self.animating_chars if not animating_char.animation_completed()
             ]
 
     def animate_chars(self, rate: float) -> None:
         for animating_char in self.animating_chars:
-            tops.print_character_at_relative_position(
-                animating_char.character, animating_char.current_x, animating_char.current_y
-            )
-            if animating_char.last_x and animating_char.last_y:
-                tops.print_character_at_relative_position(" ", animating_char.last_x, animating_char.last_y)
-            animating_char.tween()
+            tops.print_character(animating_char, clear_last=True)
+            animating_char.move()
         time.sleep(rate)
--- effects/sparkler.py
+++ effects/sparkler.py
@@ -3,7 +3,7 @@
 import time
 import random
 import utils.terminaloperations as tops
-from effects import base_effect
+from effects import effect
 from enum import Enum, auto
 
 
@@ -21,7 +21,7 @@ class SparklerPosition(Enum):
     CENTER = auto()
 
 
-class SparklerEffect(base_effect.Effect):
+class SparklerEffect(effect.Effect):
     """Effect that draws the characters spawning at varying rates from a single point."""
 
     def __init__(self, input_data: str, sparkler_position: SparklerPosition = SparklerPosition.CENTER):
@@ -38,25 +38,19 @@ class SparklerEffect(base_effect.Effect):
         """Prepares the data for the effect by starting all of the characters from a point based on SparklerPosition."""
         sparkler_origin_map = {
             SparklerPosition.CENTER: (self.input_width // 2, min(self.terminal_height // 2, self.input_height // 2)),
-            SparklerPosition.N: (self.input_width // 2, min(self.terminal_height - 1, self.input_height - 1)),
-            SparklerPosition.NW: (1, min(self.terminal_height - 1, self.input_height - 1)),
+            SparklerPosition.N: (self.input_width // 2, self.output_area_top),
+            SparklerPosition.NW: (1, self.output_area_top),
             SparklerPosition.W: (1, min(self.terminal_height // 2, self.input_height // 2)),
             SparklerPosition.SW: (1, 1),
             SparklerPosition.S: (self.input_width // 2, 1),
             SparklerPosition.SE: (self.input_width - 1, 1),
             SparklerPosition.E: (self.input_width - 1, min(self.terminal_height // 2, self.input_height // 2)),
-            SparklerPosition.NE: (self.input_width - 1, min(self.terminal_height - 1, self.input_height - 1)),
+            SparklerPosition.NE: (self.input_width - 1, self.output_area_top),
         }
 
         for character in self.characters:
-            current_x, current_y = sparkler_origin_map[self.sparkler_position]
-            self.pending_chars.append(
-                base_effect.EffectCharacter(
-                    character=character,
-                    current_x=current_x,
-                    current_y=current_y,
-                )
-            )
+            character.current_coord.column, character.current_coord.row = sparkler_origin_map[self.sparkler_position]
+            self.pending_chars.append(character)
         random.shuffle(self.pending_chars)
 
     def run(self, rate: float = 0) -> None:
@@ -75,26 +69,15 @@ class SparklerEffect(base_effect.Effect):
 
             self.animate_chars(rate)
             self.completed_chars.extend(
-                [
-                    completed_char
-                    for completed_char in self.animating_chars
-                    if completed_char.last_x == completed_char.target_x
-                    and completed_char.last_y == completed_char.target_y
-                ]
+                [completed_char for completed_char in self.animating_chars if completed_char.animation_completed()]
             )
             self.maintain_completed()
             self.animating_chars = [
-                animating_char
-                for animating_char in self.animating_chars
-                if animating_char.last_x != animating_char.target_x or animating_char.last_y != animating_char.target_y
+                animating_char for animating_char in self.animating_chars if not animating_char.animation_completed()
             ]
 
     def animate_chars(self, rate: float) -> None:
         for animating_char in self.animating_chars:
-            tops.print_character_at_relative_position(
-                animating_char.character, animating_char.current_x, animating_char.current_y
-            )
-            if animating_char.last_x and animating_char.last_y:
-                tops.print_character_at_relative_position(" ", animating_char.last_x, animating_char.last_y)
-            animating_char.tween()
+            tops.print_character(animating_char, clear_last=True)
+            animating_char.move()
         time.sleep(rate)
