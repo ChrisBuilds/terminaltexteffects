94e0653c1ac3f15e0717ab353763d81d24e93d34
Chris <741258@pm.me>
2023-07-30 19:07:46 -0500

    apply graphical modes

--- utils/terminaloperations.py
+++ utils/terminaloperations.py
@@ -38,7 +38,26 @@ def print_character(character: EffectCharacter, clear_last: bool = False) -> Non
         sys.stdout.write(ansicodes.DEC_RESTORE_CURSOR_POSITION())
         sys.stdout.flush()
 
-    move_and_print(character.symbol, character.current_coord.row, character.current_coord.column)
+    formatted_symbol = ""
+    if character.graphical_mode.bold:
+        formatted_symbol += ansicodes.APPLY_BOLD()
+    if character.graphical_mode.dim:
+        formatted_symbol += ansicodes.APPLY_DIM()
+    if character.graphical_mode.italic:
+        formatted_symbol += ansicodes.APPLY_ITALIC()
+    if character.graphical_mode.underline:
+        formatted_symbol += ansicodes.APPLY_UNDERLINE()
+    if character.graphical_mode.blink:
+        formatted_symbol += ansicodes.APPLY_BLINK()
+    if character.graphical_mode.reverse:
+        formatted_symbol += ansicodes.APPLY_REVERSE()
+    if character.graphical_mode.hidden:
+        formatted_symbol += ansicodes.APPLY_HIDDEN()
+    if character.graphical_mode.strike:
+        formatted_symbol += ansicodes.APPLY_STRIKETHROUGH()
+
+    formatted_symbol = f"{formatted_symbol}{character.symbol}{ansicodes.RESET_ALL()}"
+    move_and_print(formatted_symbol, character.current_coord.row, character.current_coord.column)
     if clear_last and character.last_coord != character.current_coord:
         move_and_print(" ", character.last_coord.row, character.last_coord.column)
 
