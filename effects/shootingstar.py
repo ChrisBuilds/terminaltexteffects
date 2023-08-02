2b86977768036a9614564832ecad6c7f68b0f9c4
Chris <741258@pm.me>
2023-08-01 22:23:04 -0500

    more effects

---
+++ effects/shootingstar.py
@@ -0,0 +1,73 @@
+import time
+import random
+import utils.terminaloperations as tops
+from effects import effect, effect_char
+
+
+class ShootingStarEffect(effect.Effect):
+    """Effect that ___."""
+
+    def __init__(self, input_data: str):
+        super().__init__(input_data)
+        self.group_by_row: dict[int, list[effect_char.EffectCharacter | None]] = {}
+
+    def prepare_data(self) -> None:
+        """Prepares the data for the effect by ___."""
+
+        for character in self.characters:
+            character.symbol = "*"
+            character.graphical_effect.color = random.randint(1, 10)
+            character.current_coord = effect_char.Coord(self.random_column(), self.output_area_top)
+            self.pending_chars.append(character)
+        for character in sorted(self.pending_chars, key=lambda c: c.final_coord.row):
+            if character.final_coord.row not in self.group_by_row:
+                self.group_by_row[character.final_coord.row] = []
+            self.group_by_row[character.final_coord.row].append(character)
+
+    def run(self, rate: float = 0) -> None:
+        """Runs the effect.
+
+        Args:
+            rate (float, optional): Time to sleep between animation steps. Defaults to 0.
+        """
+        self.prep_terminal()
+        self.prepare_data()
+        self.pending_chars.clear()
+        while self.group_by_row or self.animating_chars or self.pending_chars:
+            if not self.pending_chars and self.group_by_row:
+                self.pending_chars.extend(self.group_by_row.pop(min(self.group_by_row.keys())))  # type: ignore
+            if self.pending_chars:
+                for _ in range(random.randint(1, 3)):
+                    if self.pending_chars:
+                        self.animating_chars.append(
+                            self.pending_chars.pop(random.randint(0, len(self.pending_chars) - 1))
+                        )
+                    else:
+                        break
+            self.animate_chars(rate)
+
+            # tracking completed chars (remove if unnecessary)
+            self.completed_chars.extend(
+                [animating_char for animating_char in self.animating_chars if animating_char.animation_completed()]
+            )
+            self.maintain_completed()
+
+            # remove completed chars from animating chars
+            self.animating_chars = [
+                animating_char for animating_char in self.animating_chars if not animating_char.animation_completed()
+            ]
+
+    def animate_chars(self, rate: float) -> None:
+        """Animates the characters by calling the tween method and printing the characters to the terminal.
+
+        Args:
+            rate (float): time to sleep between animation steps
+        """
+        for animating_char in self.animating_chars:
+            tops.print_character(animating_char, clear_last=True)
+            animating_char.move()
+            if animating_char.animation_completed():
+                animating_char.symbol = animating_char.final_symbol
+                animating_char.graphical_effect = animating_char.final_graphical_effect
+
+        time.sleep(rate)
--- tte.py
+++ tte.py
@@ -1,10 +1,10 @@
 import utils.terminaloperations as tops
-from effects import expand, pour, random_sequence, scattered, sparkler, rain
+from effects import expand, pour, random_sequence, scattered, sparkler, rain, decrypt, shootingstar
 
 
 test_data = """!This
     is
-        test
+        test                    (no input was found)
             data.@
     """
 
@@ -12,15 +12,17 @@ test_data = """!This
 def main():
     input_data = tops.get_piped_input()
     if not input_data:
-        input_data = "No input data."
+        input_data = test_data
     # input_data = test_data
-    tte_effect = pour.PouringEffect(input_data, pour.PourDirection.DOWN)
+    # tte_effect = pour.PouringEffect(input_data, pour.PourDirection.DOWN)
     # tte_effect = scattered.ScatteredEffect(input_data)
     # tte_effect = expand.ExpandEffect(input_data)
     # tte_effect = random_sequence.RandomSequence(input_data)
-    # tte_effect = sparkler.SparklerEffect(input_data, sparkler.SparklerPosition.SE)
+    # tte_effect = sparkler.SparklerEffect(input_data, sparkler.SparklerPosition.E)
     # tte_effect = rain.RainEffect(input_data)
-    tte_effect.run(rate=0.01)
+    tte_effect = decrypt.DecryptEffect(input_data)
+    # tte_effect = shootingstar.ShootingStarEffect(input_data)
+    tte_effect.run(rate=0.003)
 
 
 if __name__ == "__main__":
