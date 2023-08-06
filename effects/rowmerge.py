d82703430d61909fadd7ee10a4addd3e0ff25fea
Chris <741258@pm.me>
2023-08-06 17:23:42 -0500

    new effect

---
+++ effects/rowmerge.py
@@ -0,0 +1,48 @@
+import time
+import utils.terminaloperations as tops
+from effects import effect
+
+
+class RowMergeEffect(effect.Effect):
+    """Effect that merges rows."""
+
+    def __init__(self, input_data: str, animation_rate: float = 0.03):
+        super().__init__(input_data, animation_rate)
+
+    def prepare_data(self) -> None:
+        """Prepares the data for the effect by setting every other row to start at the opposite
+        side of the terminal and reverse the order."""
+
+        self.rows = []
+        for i, row in enumerate(self.input_by_row()):
+            if i % 2 == 0:
+                row = row[::-1]
+                for c in row:
+                    c.current_coord.column = 0
+            else:
+                for c in row:
+                    c.current_coord.column = self.input_width
+            self.rows.append(row)
+
+    def run(self) -> None:
+        """Runs the effect."""
+        self.prep_terminal()
+        self.prepare_data()
+        while self.rows or self.animating_chars:
+            self.animate_chars()
+            for row in self.rows:
+                if row:
+                    self.animating_chars.append(row.pop(0))
+            self.rows = [row for row in self.rows if row]
+
+            # remove completed chars from animating chars
+            self.animating_chars = [
+                animating_char for animating_char in self.animating_chars if not animating_char.animation_completed()
+            ]
+
+    def animate_chars(self) -> None:
+        """Animates the characters by calling the move method and printing the characters to the terminal."""
+        for animating_char in self.animating_chars:
+            tops.print_character(animating_char, clear_last=True)
+            animating_char.move()
+        time.sleep(self.animation_rate)
