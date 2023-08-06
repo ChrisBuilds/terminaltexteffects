70a4a58df68f87797f343e06081f625736d05c95
Chris <741258@pm.me>
2023-08-06 08:47:58 -0500

    removes testing stuff

--- tte.py
+++ tte.py
@@ -1,28 +1,23 @@
 import utils.terminaloperations as tops
-from effects import expand, pour, random_sequence, scattered, sparkler, rain, decrypt, shootingstar
-
-
-test_data = """!This
-    is
-        test                    (no input was found)
-            data.@
-    """
+from effects import (
+    expand,
+    pour,
+    random_sequence,
+    scattered,
+    sparkler,
+    rain,
+    decrypt,
+    shootingstar,
+    rowslide,
+    columnslide,
+    verticalslice,
+)
 
 
 def main():
     input_data = tops.get_piped_input()
     if not input_data:
-        input_data = test_data
-    # input_data = test_data
-    # tte_effect = pour.PouringEffect(input_data, pour.PourDirection.DOWN)
-    # tte_effect = scattered.ScatteredEffect(input_data)
-    # tte_effect = expand.ExpandEffect(input_data)
-    # tte_effect = random_sequence.RandomSequence(input_data)
-    # tte_effect = sparkler.SparklerEffect(input_data, sparkler.SparklerPosition.E)
-    # tte_effect = rain.RainEffect(input_data)
-    tte_effect = decrypt.DecryptEffect(input_data)
-    # tte_effect = shootingstar.ShootingStarEffect(input_data)
-    tte_effect.run(rate=0.003)
+        input_data = "NO INPUT."
 
 
 if __name__ == "__main__":
