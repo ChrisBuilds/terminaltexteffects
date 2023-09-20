a053c27594b6bed2231f49dca035bb8eac57935f
Chris <741258@pm.me>
2023-09-20 08:07:59 -0500

    updates to latest

--- terminaltexteffects/template/effect_template.py
+++ terminaltexteffects/template/effect_template.py
@@ -13,7 +13,13 @@ def add_arguments(subparsers: argparse._SubParsersAction) -> None:
         subparser (argparse._SubParsersAction): subparser to add arguments to
     """
     effect_parser = subparsers.add_parser(
-        "effect_name", help="effect_description", description="effect_description", epilog="Example: effect_example"
+        "effect_name",
+        formatter_class=argtypes.CustomFormatter,
+        help="effect_description",
+        description="effect_description",
+        epilog=f"""{argtypes.EASING_EPILOG}
+
+Example: effect_example""",
     )
     effect_parser.set_defaults(effect_class=NamedEffect)
     effect_parser.add_argument(
@@ -21,14 +27,14 @@ def add_arguments(subparsers: argparse._SubParsersAction) -> None:
         "--animation-rate",
         type=argtypes.valid_animationrate,
         default=0.01,
-        help="Time between animation steps. Defaults to 0.01 seconds.",
+        help="Time, in seconds, between animation steps.",
     )
     effect_parser.add_argument(
         "--color-single",  # make more descriptive
         type=argtypes.valid_color,
         default=0,
         metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
-        help="Color for the ___. Defaults to 0",
+        help="Color for the ___.",
     )
     effect_parser.add_argument(
         "--color-list",  # make more descriptive
@@ -43,14 +49,20 @@ def add_arguments(subparsers: argparse._SubParsersAction) -> None:
         type=argtypes.valid_color,
         default="ffffff",
         metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
-        help="Color for the final character. Defaults to white.",
+        help="Color for the final character.",
     )
     effect_parser.add_argument(
         "--movement-speed",
         type=argtypes.valid_speed,
         default=1,
         metavar="(float > 0)",
-        help="Speed of the ___. Defaults to 1. Note: Speed effects the number of steps in the easing function. Adjust speed and animation rate separately to fine tune the effect.",
+        help="Speed of the ___. Note: Speed effects the number of steps in the easing function. Adjust speed and animation rate separately to fine tune the effect.",
+    )
+    effect_parser.add_argument(
+        "--easing",
+        default="IN_OUT_SINE",
+        type=argtypes.valid_ease,
+        help="Easing function to use for character movement.",
     )
 
 
@@ -58,8 +70,7 @@ class NamedEffect(base_effect.Effect):
     """Effect that ___."""
 
     def __init__(self, terminal: Terminal, args: argparse.Namespace):
-        super().__init__(terminal)
-        self.args = args
+        super().__init__(terminal, args)
 
     def prepare_data(self) -> None:
         """Prepares the data for the effect by ___."""
