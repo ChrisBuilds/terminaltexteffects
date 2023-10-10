f970a6c6cd5837a77957fe6597016c220c57bdec
Chris <741258@pm.me>
2023-10-10 06:02:44 -0500

    Adds docs and synced animation.

--- terminaltexteffects/effects/effect_middleout.py
+++ terminaltexteffects/effects/effect_middleout.py
@@ -16,11 +16,13 @@ def add_arguments(subparsers: argparse._SubParsersAction) -> None:
     effect_parser = subparsers.add_parser(
         "middleout",
         formatter_class=argtypes.CustomFormatter,
-        help="effect_description",
-        description="effect_description",
+        help="Text expands in a single row or column in the middle of the output area then out.",
+        description="Text expands in a single row or column in the middle of the output area then out.",
         epilog=f"""{argtypes.EASING_EPILOG}
 
-Example: effect_example""",
+Example: terminaltexteffects middleout -a 0.01 --expand-direction horizontal --center-movement-speed 0.35 
+         --full-movement-speed 0.35 --center-easing IN_OUT_SINE --full-easing IN_OUT_SINE 
+         --center-expand-color 00ff00 --full-expand-color 0000ff --starting-color ff0000""",
     )
     effect_parser.set_defaults(effect_class=MiddleoutEffect)
     effect_parser.add_argument(
@@ -31,32 +33,25 @@ Example: effect_example""",
         help="Time, in seconds, between animation steps.",
     )
     effect_parser.add_argument(
-        "--starting-color",  # make more descriptive
-        type=argtypes.valid_color,
-        default="ff0000",
-        metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
-        help="Color for the ___.",
-    )
-    effect_parser.add_argument(
-        "--center-expand-color",  # make more descriptive
-        type=argtypes.valid_color,
-        default="00ff00",
-        metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
-        help="Color for the ___.",
-    )
-    effect_parser.add_argument(
-        "--full-expand-color",  # make more descriptive
-        type=argtypes.valid_color,
-        default="0000ff",
-        metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
-        help="Color for the ___.",
-    )
-    effect_parser.add_argument(
-        "--final-color",
+        "--starting-color",
         type=argtypes.valid_color,
         default="ffffff",
         metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
-        help="Color for the final character.",
+        help="Color for the initial text in the center of the output area.",
+    )
+    effect_parser.add_argument(
+        "--center-expand-color",
+        type=argtypes.valid_color,
+        default="ffffff",
+        metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
+        help="Color for the row or column as it expands.",
+    )
+    effect_parser.add_argument(
+        "--full-expand-color",
+        type=argtypes.valid_color,
+        default="ffffff",
+        metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
+        help="Color for the full text as it expands.",
     )
     effect_parser.add_argument(
         "--expand-direction",
@@ -99,10 +94,9 @@ class MiddleoutEffect(base_effect.Effect):
         super().__init__(terminal, args)
         self.center_gradient = graphics.Gradient(self.args.starting_color, self.args.center_expand_color, 10)
         self.full_gradient = graphics.Gradient(self.args.center_expand_color, self.args.full_expand_color, 10)
-        self.final_gradient = graphics.Gradient(self.args.full_expand_color, self.args.final_color, 10)
 
     def prepare_data(self) -> None:
-        """Prepares the data for the effect by ___."""
+        """Prepares the data for the effect."""
         for character in self.terminal.characters:
             character.motion.set_coordinate(
                 self.terminal.output_area.center_column, self.terminal.output_area.center_row
@@ -112,8 +106,7 @@ class MiddleoutEffect(base_effect.Effect):
                 character.animation.add_effect_to_scene("center", character.input_symbol, step, 2)
             for step in self.full_gradient:
                 character.animation.add_effect_to_scene("full", character.input_symbol, step, 2)
-            for step in self.final_gradient:
-                character.animation.add_effect_to_scene("final", character.input_symbol, step, 2)
+            character.animation.scenes["full"].waypoint_sync_id = "full"
             character.animation.activate_scene("center")
 
             # setup waypoints
@@ -131,7 +124,7 @@ class MiddleoutEffect(base_effect.Effect):
                 ease=self.args.center_easing,
             )
             character.motion.new_waypoint(
-                "final",
+                "full",
                 character.input_coord.column,
                 character.input_coord.row,
                 speed=self.args.full_movement_speed,
@@ -151,7 +144,7 @@ class MiddleoutEffect(base_effect.Effect):
             if all([character.motion.active_waypoint == None for character in self.animating_chars]):
                 final = True
                 for character in self.animating_chars:
-                    character.motion.activate_waypoint("final")
+                    character.motion.activate_waypoint("full")
                     character.animation.activate_scene("full")
             self.terminal.print()
             self.animate_chars()
@@ -166,5 +159,5 @@ class MiddleoutEffect(base_effect.Effect):
     def animate_chars(self) -> None:
         """Animates the characters by calling the move method and step animation."""
         for animating_char in self.animating_chars:
-            animating_char.animation.step_animation()
             animating_char.motion.move()
+            animating_char.animation.step_animation()
