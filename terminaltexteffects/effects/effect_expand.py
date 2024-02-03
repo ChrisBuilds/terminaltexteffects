e7c2e86b1096784fc5b76b512b53687d1f874a8d
Chris <741258+github@pm.me>
2024-02-03 11:13:10 -0600

    Add gradient options to ExpandEffect

--- terminaltexteffects/effects/effect_expand.py
+++ terminaltexteffects/effects/effect_expand.py
@@ -3,6 +3,7 @@ import argparse
 import terminaltexteffects.utils.argtypes as argtypes
 from terminaltexteffects.base_character import EffectCharacter, EventHandler
 from terminaltexteffects.utils.terminal import Terminal
+from terminaltexteffects.utils import graphics
 
 
 def add_arguments(subparsers: argparse._SubParsersAction) -> None:
@@ -29,6 +30,28 @@ Example: terminaltexteffects expand -a 0.01 --movement-speed 0.5 --easing IN_OUT
         help="Minimum time, in seconds, between animation steps. This value does not normally need to be modified. Use this to increase the playback speed of all aspects of the effect. This will have no impact beyond a certain lower threshold due to the processing speed of your device.",
     )
     effect_parser.add_argument(
+        "--gradient-stops",
+        type=argtypes.color,
+        nargs="*",
+        default=[],
+        metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
+        help="Space separated, unquoted, list of colors for the character gradient. If only one color is provided, the characters will be displayed in that color.",
+    )
+    effect_parser.add_argument(
+        "--gradient-steps",
+        type=argtypes.positive_int,
+        default=10,
+        metavar="(int > 0)",
+        help="Number of gradient steps to use. More steps will create a smoother and longer gradient animation.",
+    )
+    effect_parser.add_argument(
+        "--gradient-frames",
+        type=argtypes.positive_int,
+        default=5,
+        metavar="(int > 0)",
+        help="Number of frames to display each gradient step.",
+    )
+    effect_parser.add_argument(
         "--movement-speed",
         type=argtypes.positive_float,
         default=0.5,
@@ -51,9 +74,12 @@ class ExpandEffect:
         self.args = args
         self.pending_chars: list[EffectCharacter] = []
         self.animating_chars: list[EffectCharacter] = []
+        self.gradient_stops: list[int | str] = self.args.gradient_stops
 
     def prepare_data(self) -> None:
         """Prepares the data for the effect by starting all of the characters from a point in the middle of the input data."""
+        if len(self.gradient_stops) > 1:
+            gradient = graphics.Gradient(self.gradient_stops, self.args.gradient_steps)
 
         for character in self.terminal.characters:
             character.motion.set_coordinate(self.terminal.output_area.center)
@@ -72,6 +98,14 @@ class ExpandEffect:
             character.event_handler.register_event(
                 EventHandler.Event.PATH_COMPLETE, input_coord_path, EventHandler.Action.SET_LAYER, 0
             )
+            if self.gradient_stops:
+                gradient_scn = character.animation.new_scene("gradient_scn")
+                if len(self.gradient_stops) > 1:
+                    for step in gradient:
+                        gradient_scn.add_frame(character.input_symbol, self.args.gradient_frames, color=step)
+                else:
+                    gradient_scn.add_frame(character.input_symbol, 1, color=self.gradient_stops[0])
+                character.animation.activate_scene(gradient_scn)
 
     def run(self) -> None:
         """Runs the effect."""
