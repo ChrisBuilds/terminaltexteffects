f26ae1722ec2199dbd03d0b85a0e1f23ba305095
Chris <741258+github@pm.me>
2024-02-03 06:58:55 -0600

    updates tests

--- tests/test_tte.py
+++ tests/test_tte.py
@@ -65,6 +65,9 @@ def make_args() -> Namespace:
     args.animation_rate = 0
     args.movement_speed = 3
     args.easing = easing.in_sine
+    args.gradient_stops = [10, 11, 12]
+    args.gradient_steps = 5
+    args.gradient_frames = 5
     return args
 
 
@@ -72,11 +75,9 @@ def test_slide_effect() -> None:
     for input_data in test_inputs:
         args = make_args()
         args.grouping = "diagonal"
-        args.gradient = [10, 11, 12]
         args.gap = 1
         args.merge = True
         args.reverse_direction = False
-        args.gradient_steps = 5
         args.easing = easing.out_quad
         terminal = Terminal(input_data, args)
         slide_effect = effect_slide.SlideEffect(terminal, args)
@@ -88,8 +89,6 @@ def test_wipe_effect() -> None:
         args = make_args()
         args.wipe_direction = "column_left_to_right"
         args.wipe_delay = 0
-        args.gradient = [10, 11, 12]
-        args.gradient_steps = 5
         terminal = Terminal(input_data, args)
         wipe_effect = effect_wipe.WipeEffect(terminal, args)
         wipe_effect.run()
@@ -263,6 +262,7 @@ def test_pour_effect() -> None:
     for input_data in test_inputs:
         args = make_args()
         args.pour_direction = effect_pour.PourDirection.DOWN
+        args.gap = 2
         terminal = Terminal(input_data, args)
         pour_effect = effect_pour.PourEffect(terminal, args)
         pour_effect.run()
