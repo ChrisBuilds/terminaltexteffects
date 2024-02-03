c3d21ac09a5455e063579a23f03ae8031ecba041
Chris <741258+github@pm.me>
2024-02-03 06:52:28 -0600

    Pour back and forth, adds gradient args.

--- terminaltexteffects/effects/effect_pour.py
+++ terminaltexteffects/effects/effect_pour.py
@@ -6,7 +6,7 @@ from enum import Enum, auto
 import terminaltexteffects.utils.argtypes as argtypes
 from terminaltexteffects.base_character import EffectCharacter
 from terminaltexteffects.utils.terminal import Terminal
-from terminaltexteffects.utils import motion
+from terminaltexteffects.utils import motion, graphics
 
 
 def add_arguments(subparsers: argparse._SubParsersAction) -> None:
@@ -46,6 +46,35 @@ Example: terminaltexteffects pour -a 0.004 --pour-direction down""",
         help="Movement speed of the characters. Note: Speed effects the number of steps in the easing function. Adjust speed and animation rate separately to fine tune the effect.",
     )
     effect_parser.add_argument(
+        "--gap",
+        type=argtypes.nonnegative_int,
+        default=1,
+        metavar="(int >= 0)",
+        help="Number of frames to wait between each character in the pour effect. Increase to slow down effect and create a more defined back and forth motion.",
+    )
+    effect_parser.add_argument(
+        "--gradient-stops",
+        type=argtypes.color,
+        nargs="*",
+        default=[],
+        metavar="(XTerm [0-255] OR RGB Hex [000000-ffffff])",
+        help="Space separated, unquoted, list of colors for the character gradient. If only one color is provided, the characters will be displayed in that color.",
+    )
+    effect_parser.add_argument(
+        "--gradient-steps",
+        type=argtypes.positive_int,
+        default=10,
+        metavar="(int > 0)",
+        help="Number of gradient steps to use. More steps will create a smoother and longer gradient animation.",
+    )
+    effect_parser.add_argument(
+        "--gradient-frames",
+        type=argtypes.positive_int,
+        default=5,
+        metavar="(int > 0)",
+        help="Number of frames to display each gradient step.",
+    )
+    effect_parser.add_argument(
         "--easing",
         default="IN_QUAD",
         type=argtypes.ease,
@@ -66,7 +95,7 @@ class PourEffect:
     def __init__(self, terminal: Terminal, args: argparse.Namespace):
         self.terminal = terminal
         self.args = args
-        self.pending_chars: list[EffectCharacter] = []
+        self.pending_groups: list[list[EffectCharacter]] = []
         self.animating_chars: list[EffectCharacter] = []
         self.pour_direction = {
             "down": PourDirection.DOWN,
@@ -74,55 +103,83 @@ class PourEffect:
             "left": PourDirection.LEFT,
             "right": PourDirection.RIGHT,
         }.get(args.pour_direction, PourDirection.DOWN)
+        self.gradient_stops: list[int | str] = self.args.gradient_stops
 
     def prepare_data(self) -> None:
         """Prepares the data for the effect by sorting the characters by the pour direction."""
         sort_map = {
-            PourDirection.DOWN: lambda character: character.input_coord.row,
-            PourDirection.UP: lambda character: -character.input_coord.row,
-            PourDirection.LEFT: lambda character: character.input_coord.column,
-            PourDirection.RIGHT: lambda character: -character.input_coord.column,
+            PourDirection.DOWN: Terminal.CharacterSort.ROW_BOTTOM_TO_TOP,
+            PourDirection.UP: Terminal.CharacterSort.ROW_TOP_TO_BOTTOM,
+            PourDirection.LEFT: Terminal.CharacterSort.COLUMN_LEFT_TO_RIGHT,
+            PourDirection.RIGHT: Terminal.CharacterSort.COLUMN_RIGHT_TO_LEFT,
         }
-        self.terminal.characters.sort(key=sort_map[self.pour_direction])
-        for character in self.terminal.characters:
-            character.is_active = False
-            if self.pour_direction == PourDirection.DOWN:
-                character.motion.set_coordinate(
-                    motion.Coord(character.input_coord.column, self.terminal.output_area.top)
+        if len(self.gradient_stops) > 1:
+            gradient = graphics.Gradient(self.gradient_stops, self.args.gradient_steps)
+        groups = self.terminal.get_characters(sort_order=sort_map[self.pour_direction])
+        for i, group in enumerate(groups):
+            for character in group:
+                character.is_active = False
+                if self.pour_direction == PourDirection.DOWN:
+                    character.motion.set_coordinate(
+                        motion.Coord(character.input_coord.column, self.terminal.output_area.top)
+                    )
+                elif self.pour_direction == PourDirection.UP:
+                    character.motion.set_coordinate(
+                        motion.Coord(character.input_coord.column, self.terminal.output_area.bottom)
+                    )
+                elif self.pour_direction == PourDirection.LEFT:
+                    character.motion.set_coordinate(
+                        motion.Coord(self.terminal.output_area.right, character.input_coord.row)
+                    )
+                elif self.pour_direction == PourDirection.RIGHT:
+                    character.motion.set_coordinate(
+                        motion.Coord(self.terminal.output_area.left, character.input_coord.row)
+                    )
+                input_coord_path = character.motion.new_path(
+                    "input_coord",
+                    speed=self.args.movement_speed,
+                    ease=self.args.easing,
                 )
-            elif self.pour_direction == PourDirection.UP:
-                character.motion.set_coordinate(
-                    motion.Coord(character.input_coord.column, self.terminal.output_area.bottom)
-                )
-            elif self.pour_direction == PourDirection.LEFT:
-                character.motion.set_coordinate(
-                    motion.Coord(self.terminal.output_area.right, character.input_coord.row)
-                )
-            elif self.pour_direction == PourDirection.RIGHT:
-                character.motion.set_coordinate(motion.Coord(self.terminal.output_area.left, character.input_coord.row))
-            input_coord_path = character.motion.new_path(
-                "input_coord",
-                speed=self.args.movement_speed,
-                ease=self.args.easing,
-            )
-            input_coord_wpt = input_coord_path.new_waypoint("input_coord", character.input_coord)
-            character.motion.activate_path(input_coord_path)
-            self.pending_chars.append(character)
+                input_coord_wpt = input_coord_path.new_waypoint("input_coord", character.input_coord)
+                character.motion.activate_path(input_coord_path)
+            if i % 2 == 0:
+                self.pending_groups.append(group)
+            else:
+                self.pending_groups.append(group[::-1])
+            if self.gradient_stops:
+                for character in group:
+                    gradient_scn = character.animation.new_scene("gradient_scn")
+                    if len(self.gradient_stops) > 1:
+                        for step in gradient:
+                            gradient_scn.add_frame(character.input_symbol, self.args.gradient_frames, color=step)
+                    else:
+                        gradient_scn.add_frame(character.input_symbol, 1, color=self.gradient_stops[0])
+                    character.animation.activate_scene(gradient_scn)
 
     def run(self) -> None:
         """Runs the effect."""
         self.prepare_data()
         self.terminal.print()
-        while self.pending_chars or self.animating_chars:
-            if self.pending_chars:
-                next_character = self.pending_chars.pop(0)
-                next_character.is_active = True
-                self.animating_chars.append(next_character)
+        current_group = self.pending_groups.pop(0)
+        gap = 0
+        while self.pending_groups or self.animating_chars or current_group:
+            if not current_group:
+                if self.pending_groups:
+                    current_group = self.pending_groups.pop(0)
+            if current_group:
+                if not gap:
+                    next_character = current_group.pop(0)
+                    next_character.is_active = True
+                    self.animating_chars.append(next_character)
+                    gap = self.args.gap
+                else:
+                    gap -= 1
             self.animate_chars()
             self.animating_chars = [
                 animating_char
                 for animating_char in self.animating_chars
                 if not animating_char.motion.movement_is_complete()
+                or not animating_char.animation.active_scene_is_complete()
             ]
             self.terminal.print()
 
@@ -130,3 +187,4 @@ class PourEffect:
         """Animates the sliding characters."""
         for animating_char in self.animating_chars:
             animating_char.motion.move()
+            animating_char.animation.step_animation()
