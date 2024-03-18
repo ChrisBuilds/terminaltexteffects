dd3e9ecacc550297a7fb54e7878f048b9b9c092c
Chris <741258+github@pm.me>
2024-03-17 21:12:46 -0500

    adds store_true and store_false handling and choices metadata var.

--- terminaltexteffects/utils/argsdataclass.py
+++ terminaltexteffects/utils/argsdataclass.py
@@ -1,7 +1,7 @@
 import argparse
 import inspect
-from dataclasses import Field, dataclass, fields, MISSING
 import typing
+from dataclasses import MISSING, Field, dataclass, fields
 
 
 class ArgField(Field):
@@ -11,10 +11,11 @@ class ArgField(Field):
         cmd_name: str | list[str],
         help: str,
         type_parser: typing.Any = None,
-        metavar: str|None = None,
-        nargs: str|None = None,
-        action: str|None = None,
+        metavar: str | None = None,
+        nargs: str | None = None,
+        action: str | None = None,
         required: bool = False,
+        choices: list[str | int] | None = None,
         # python internal attrs
         default=MISSING,
         default_factory=MISSING,
@@ -32,40 +33,47 @@ class ArgField(Field):
             help=help,
             action=action,
             required=required,
+            choices=choices,
         )
+        if action == "store_true":
+            default = False
+        elif action == "store_false":
+            default = True
 
         super().__init__(default, default_factory, init, repr, hash, compare, vars(additional_metadata), kw_only)
 
     @dataclass
     class FieldAdditionalMetaData:
         cmd_name: str | list[str]
-        type_parser: typing.Any|None = None
-        metavar: str|None = None
-        nargs: str|None = None
-        help: str|None = None
-        action: str|None = None
+        type_parser: typing.Any | None = None
+        metavar: str | None = None
+        nargs: str | None = None
+        help: str | None = None
+        action: str | None = None
         required: bool = False
-        
-        
+        choices: list[str | int] | None = None
+
+
 @dataclass
 class ArgParserDescriptor:
     """This dataclass contains required attributes to call "add_parser()" method of
     _argparse._SubParsersAction" class
     """
+
     name: str
     formatter_class: typing.Any
     help: str
     description: str
-    epilog: str        
+    epilog: str
 
 
 @dataclass
 class ArgsDataClass:
     @classmethod
-    def from_parsed_args_mapping(selfClass, parsed_args: argparse.Namespace, arg_class=None):
+    def from_parsed_args_mapping(cls, parsed_args: argparse.Namespace, arg_class=None):
         # parsed_args.
         if arg_class is None:
-            arg_class= parsed_args.arg_class
+            arg_class = parsed_args.arg_class
 
         signature = inspect.signature(arg_class.__init__)
         parameters = signature.parameters
@@ -73,29 +81,32 @@ class ArgsDataClass:
         for param in parameters:
             if param == "self":
                 continue
-            params_dict[param] = getattr(parsed_args, param)
+            param_value = getattr(parsed_args, param)
+            if isinstance(param_value, list):  # argparser returns list for nargs="+" or nargs="*"
+                param_value = tuple(param_value)
+            params_dict[param] = param_value
 
         new_instance = arg_class(**params_dict)
         return new_instance
 
     @classmethod
-    def get_All_fields(selfClass) -> dict[str, Field]:
+    def get_All_fields(cls) -> dict[str, Field]:
         fields_list = {}
-        for f in fields(selfClass):
+        for f in fields(cls):
             fields_list[f.name] = f
 
         return fields_list
 
     @classmethod
-    def add_args_to_parser(selfClass, parser):
-        args = selfClass.get_All_fields()
+    def add_args_to_parser(cls, parser):
+        args = cls.get_All_fields()
         for arg in args.values():
             if not arg.metadata:
                 continue
             additional_metadata = ArgField.FieldAdditionalMetaData(**arg.metadata)
             if isinstance(additional_metadata.cmd_name, str):
                 additional_metadata.cmd_name = [additional_metadata.cmd_name]
-            field_names_mapping= {"type_parser":"type"}
+            field_names_mapping = {"type_parser": "type"}
             arg_descriptor = {}
             for attr_name in vars(additional_metadata):
                 value = getattr(additional_metadata, attr_name)
@@ -104,26 +115,26 @@ class ArgsDataClass:
                 if attr_name == "cmd_name":
                     continue
                 if attr_name in field_names_mapping:
-                    attr_name= field_names_mapping[attr_name]
-                    
+                    attr_name = field_names_mapping[attr_name]
+
                 arg_descriptor[attr_name] = value
 
             parser.add_argument(*additional_metadata.cmd_name, **arg_descriptor, default=arg.default)
 
     @classmethod
-    def add_to_args_subparsers(selfClass, subparsers: argparse._SubParsersAction):
+    def add_to_args_subparsers(cls, subparsers: argparse._SubParsersAction):
         """Adds arguments to the subparser.
 
         Args:
             arg_data_class (ArgsDataClass): ArgDataClass that required args defined in it
             subparser (argparse._SubParsersAction): subparser to add arguments to
         """
-        sub_parser_descriptor= getattr(selfClass,"arg_class_metadata")
+        sub_parser_descriptor = getattr(cls, "arg_class_metadata")
         new_parser = subparsers.add_parser(**vars(sub_parser_descriptor))
-        new_parser.set_defaults(arg_class=selfClass)
-        selfClass.add_args_to_parser(new_parser)
-        
-    #arg_class_metadata:ArgParserDescriptor|None= None
+        new_parser.set_defaults(arg_class=cls)
+        cls.add_args_to_parser(new_parser)
+
+    # arg_class_metadata:ArgParserDescriptor|None= None
 
 
 def argclass(name: str, formatter_class: typing.Any, help: str, description: str, epilog: str):
@@ -136,6 +147,7 @@ def argclass(name: str, formatter_class: typing.Any, help: str, description: str
         description (str): description string for parser or subparser
         epilog (str): epilog string for parser or subparser
     """
+
     def decorator(cls):
         cls.arg_class_metadata = ArgParserDescriptor(
             name=name, formatter_class=formatter_class, help=help, description=description, epilog=epilog
