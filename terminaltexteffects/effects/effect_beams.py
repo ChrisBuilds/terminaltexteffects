9ec5bdfe807bbee9564ced11c2db6d79058df37b
Chris <741258+github@pm.me>
2024-03-20 07:57:13 -0500

    Adds gradient direction

--- terminaltexteffects/effects/effect_beams.py
+++ terminaltexteffects/effects/effect_beams.py
@@ -103,6 +103,13 @@ class BeamsEffectArgs(ArgsDataClass):
         metavar=arg_validators.PositiveInt.METAVAR,
         help="Number of frames to display each gradient step.",
     )  # type: ignore[assignment]
+    final_gradient_direction: graphics.Gradient.Direction = ArgField(
+        cmd_name="--final-gradient-direction",
+        type_parser=arg_validators.GradientDirection.type_parser,
+        default=graphics.Gradient.Direction.VERTICAL,
+        metavar=arg_validators.GradientDirection.METAVAR,
+        help="Direction of the gradient for the final color.",
+    )  # type: ignore[assignment]
     final_wipe_speed: int = ArgField(
         cmd_name="--final-wipe-speed",
         type_parser=arg_validators.PositiveInt.type_parser,
@@ -165,11 +172,11 @@ class BeamsEffect:
 
     def prepare_data(self) -> None:  # testing
         final_gradient = graphics.Gradient(*self.args.final_gradient_stops, steps=self.args.final_gradient_steps)
-
+        final_gradient_mapping = final_gradient.build_coordinate_color_mapping(
+            self.terminal.output_area.top, self.terminal.output_area.right, self.args.final_gradient_direction
+        )
         for character in self.terminal.get_characters(fill_chars=True):
-            self.character_final_color_map[character] = final_gradient.get_color_at_fraction(
-                character.input_coord.row / self.terminal.output_area.top
-            )
+            self.character_final_color_map[character] = final_gradient_mapping[character.input_coord]
 
         beam_gradient = graphics.Gradient(*self.args.beam_gradient_stops, steps=self.args.beam_gradient_steps)
         groups: list[Group] = []
