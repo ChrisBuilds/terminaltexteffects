fc0f9d0d82ce115cd8d287b0e9e732fa2bb2a979
Chris <741258+github@pm.me>
2024-03-24 07:21:17 -0500

    Adds gradient direction and changes default colors.

--- terminaltexteffects/effects/effect_scattered.py
+++ terminaltexteffects/effects/effect_scattered.py
@@ -22,28 +22,37 @@ Example: terminaltexteffects scattered --gradient-stops 8A008A 00D1FF FFFFFF --g
 )
 @dataclass
 class ScatteredEffectArgs(ArgsDataClass):
-    gradient_stops: tuple[graphics.Color, ...] = ArgField(
-        cmd_name=["--gradient-stops"],
+    final_gradient_stops: tuple[graphics.Color, ...] = ArgField(
+        cmd_name=["--final-gradient-stops"],
         type_parser=arg_validators.Color.type_parser,
         nargs="+",
-        default=("8A008A", "00D1FF", "FFFFFF"),
+        default=("ff9048", "ab9dff", "bdffea"),
         metavar=arg_validators.Color.METAVAR,
         help="Space separated, unquoted, list of colors for the character gradient. If only one color is provided, the characters will be displayed in that color.",
     )  # type: ignore[assignment]
-    gradient_steps: tuple[int, ...] = ArgField(
-        cmd_name="--gradient-steps",
+    final_gradient_steps: tuple[int, ...] = ArgField(
+        cmd_name="--final-gradient-steps",
         type_parser=arg_validators.PositiveInt.type_parser,
         default=(12,),
         metavar=arg_validators.PositiveInt.METAVAR,
         help="Number of gradient steps to use. More steps will create a smoother and longer gradient animation.",
     )  # type: ignore[assignment]
-    gradient_frames: int = ArgField(
-        cmd_name="--gradient-frames",
+    final_gradient_frames: int = ArgField(
+        cmd_name="--final-gradient-frames",
         type_parser=arg_validators.PositiveInt.type_parser,
         default=12,
         metavar=arg_validators.PositiveInt.METAVAR,
         help="Number of frames to display each gradient step.",
     )  # type: ignore[assignment]
+
+    final_gradient_direction: graphics.Gradient.Direction = ArgField(
+        cmd_name="--final-gradient-direction",
+        type_parser=arg_validators.GradientDirection.type_parser,
+        default=graphics.Gradient.Direction.VERTICAL,
+        metavar=arg_validators.GradientDirection.METAVAR,
+        help="Direction of the gradient for the final color.",
+    )  # type: ignore[assignment]
+
     movement_speed: float = ArgField(
         cmd_name="--movement-speed",
         type_parser=arg_validators.PositiveFloat.type_parser,
@@ -75,13 +84,12 @@ class ScatteredEffect:
 
     def prepare_data(self) -> None:
         """Prepares the data for the effect by scattering the characters within range of the input width and height."""
-        final_gradient = graphics.Gradient(*self.args.gradient_stops, steps=self.args.gradient_steps)
-
+        final_gradient = graphics.Gradient(*self.args.final_gradient_stops, steps=self.args.final_gradient_steps)
+        final_gradient_mapping = final_gradient.build_coordinate_color_mapping(
+            self.terminal.output_area.top, self.terminal.output_area.right, self.args.final_gradient_direction
+        )
         for character in self.terminal.get_characters():
-            self.character_final_color_map[character] = final_gradient.get_color_at_fraction(
-                character.input_coord.row / self.terminal.output_area.top
-            )
-        for character in self.terminal.get_characters():
+            self.character_final_color_map[character] = final_gradient_mapping[character.input_coord]
             if self.terminal.output_area.right < 2 or self.terminal.output_area.top < 2:
                 character.motion.set_coordinate(Coord(1, 1))
             else:
@@ -100,7 +108,9 @@ class ScatteredEffect:
             char_gradient = graphics.Gradient(
                 final_gradient.spectrum[0], self.character_final_color_map[character], steps=10
             )
-            gradient_scn.apply_gradient_to_symbols(char_gradient, character.input_symbol, self.args.gradient_frames)
+            gradient_scn.apply_gradient_to_symbols(
+                char_gradient, character.input_symbol, self.args.final_gradient_frames
+            )
             character.animation.activate_scene(gradient_scn)
             self.active_chars.append(character)
 
