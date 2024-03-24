5d259d8d18d09d19ab6f36857e110663e04ff7ef
Chris <741258+github@pm.me>
2024-03-24 12:55:29 -0500

    Adds gradient direction.

--- terminaltexteffects/effects/effect_unstable.py
+++ terminaltexteffects/effects/effect_unstable.py
@@ -48,6 +48,13 @@ class UnstableEffectArgs(ArgsDataClass):
         metavar=arg_validators.PositiveInt.METAVAR,
         help="Space separated, unquoted, list of the number of gradient steps to use. More steps will create a smoother and longer gradient animation.",
     )  # type: ignore[assignment]
+    final_gradient_direction: graphics.Gradient.Direction = ArgField(
+        cmd_name="--final-gradient-direction",
+        type_parser=arg_validators.GradientDirection.type_parser,
+        default=graphics.Gradient.Direction.VERTICAL,
+        metavar=arg_validators.GradientDirection.METAVAR,
+        help="Direction of the gradient for the final color.",
+    )  # type: ignore[assignment]
     explosion_ease: easing.EasingFunction = ArgField(
         cmd_name=["--explosion-ease"],
         type_parser=arg_validators.Ease.type_parser,
@@ -97,12 +104,11 @@ class UnstableEffect:
         choosing a location on the perimeter of the output area for the character to travel
         after exploding. Creates all waypoints and scenes for the characters."""
         final_gradient = graphics.Gradient(*self.args.final_gradient_stops, steps=self.args.final_gradient_steps)
-
+        final_gradient_mapping = final_gradient.build_coordinate_color_mapping(
+            self.terminal.output_area.top, self.terminal.output_area.right, self.args.final_gradient_direction
+        )
         for character in self.terminal.get_characters():
-            self.character_final_color_map[character] = final_gradient.get_color_at_fraction(
-                character.input_coord.row / self.terminal.output_area.top
-            )
-
+            self.character_final_color_map[character] = final_gradient_mapping[character.input_coord]
         character_coords = [character.input_coord for character in self.terminal.get_characters()]
         for character in self.terminal.get_characters():
             pos = random.randint(0, 3)
