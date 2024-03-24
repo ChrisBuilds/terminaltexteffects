8036ced9cd34ab88ef27352ed5cbfafc421e6f90
Chris <741258+github@pm.me>
2024-03-24 12:20:06 -0500

    Adds gradient direction.

--- terminaltexteffects/effects/effect_spray.py
+++ terminaltexteffects/effects/effect_spray.py
@@ -44,6 +44,13 @@ class SprayEffectArgs(ArgsDataClass):
         metavar=arg_validators.PositiveInt.METAVAR,
         help="Space separated, unquoted, list of the number of gradient steps to use. More steps will create a smoother and longer gradient animation.",
     )  # type: ignore[assignment]
+    final_gradient_direction: graphics.Gradient.Direction = ArgField(
+        cmd_name="--final-gradient-direction",
+        type_parser=arg_validators.GradientDirection.type_parser,
+        default=graphics.Gradient.Direction.VERTICAL,
+        metavar=arg_validators.GradientDirection.METAVAR,
+        help="Direction of the gradient for the final color.",
+    )  # type: ignore[assignment]
     spray_position: str = ArgField(
         cmd_name="--spray-position",
         choices=["n", "ne", "e", "se", "s", "sw", "w", "nw", "center"],
@@ -124,11 +131,11 @@ class SprayEffect:
     def prepare_data(self) -> None:
         """Prepares the data for the effect by starting all of the characters from a point based on SparklerPosition."""
         final_gradient = graphics.Gradient(*self.args.final_gradient_stops, steps=self.args.final_gradient_steps)
-
+        final_gradient_mapping = final_gradient.build_coordinate_color_mapping(
+            self.terminal.output_area.top, self.terminal.output_area.right, self.args.final_gradient_direction
+        )
         for character in self.terminal.get_characters():
-            self.character_final_color_map[character] = final_gradient.get_color_at_fraction(
-                character.input_coord.row / self.terminal.output_area.top
-            )
+            self.character_final_color_map[character] = final_gradient_mapping[character.input_coord]
         spray_origin_map = {
             SprayPosition.CENTER: (self.terminal.output_area.center),
             SprayPosition.N: Coord(self.terminal.output_area.right // 2, self.terminal.output_area.top),
