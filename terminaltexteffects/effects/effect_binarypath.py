9ef0d83c9f8332ef7f07f90322aea2b82fd54d81
Chris <741258+github@pm.me>
2024-03-25 06:14:04 -0500

    Updates example

--- terminaltexteffects/effects/effect_binarypath.py
+++ terminaltexteffects/effects/effect_binarypath.py
@@ -19,7 +19,7 @@ def get_effect_and_args() -> tuple[type[typing.Any], type[ArgsDataClass]]:
     formatter_class=arg_validators.CustomFormatter,
     help="Binary representations of each character move through the terminal towards the home coordinate of the character.",
     description="Binary representations of each character move through the terminal towards the home coordinate of the character.",
-    epilog="""Example: terminaltexteffects binarypath --final-gradient-stops 8A008A 00D1FF FFFFFF --final-gradient-steps 12 --binary-colors 044E29 157e38 45bf55 95ed87 --movement-speed 1.0 --active-binary-groups 0.05""",
+    epilog="""Example: terminaltexteffects binarypath --final-gradient-stops 00d500 007500 --final-gradient-steps 12 --final-gradient-direction vertical --binary-colors 044E29 157e38 45bf55 95ed87 --movement-speed 1.0 --active-binary-groups 0.05""",
 )
 @dataclass
 class BinaryPathEffectArgs(ArgsDataClass):
