e40cd2822cebc6be56503ea8283c77c9708cbe76
Chris <741258+github@pm.me>
2024-04-22 05:51:22 -0500

    library refactor

--- terminaltexteffects/effects/effect_blackhole.py
+++ terminaltexteffects/effects/effect_blackhole.py
@@ -1,19 +1,17 @@
 import random
 import typing
-from collections.abc import Iterator
 from dataclasses import dataclass
 
 import terminaltexteffects.utils.arg_validators as arg_validators
 from terminaltexteffects.base_character import EffectCharacter, EventHandler
-from terminaltexteffects.base_effect import BaseEffect
+from terminaltexteffects.base_effect import BaseEffect, BaseEffectIterator
 from terminaltexteffects.utils import animation, easing, geometry, graphics
 from terminaltexteffects.utils.argsdataclass import ArgField, ArgsDataClass, argclass
 from terminaltexteffects.utils.geometry import Coord
-from terminaltexteffects.utils.terminal import Terminal, TerminalConfig
 
 
 def get_effect_and_args() -> tuple[type[typing.Any], type[ArgsDataClass]]:
-    return BlackholeEffect, EffectConfig
+    return Blackhole, BlackholeConfig
 
 
 @argclass(
@@ -23,7 +21,7 @@ def get_effect_and_args() -> tuple[type[typing.Any], type[ArgsDataClass]]:
     epilog="""Example: terminaltexteffects blackhole --star-colors ffcc0d ff7326 ff194d bf2669 702a8c 049dbf --final-gradient-stops 8A008A 00D1FF FFFFFF --final-gradient-steps 12 --final-gradient-direction vertical""",
 )
 @dataclass
-class EffectConfig(ArgsDataClass):
+class BlackholeConfig(ArgsDataClass):
     """Configuration for the Blackhole effect.
 
     Attributes:
@@ -88,42 +86,27 @@ class EffectConfig(ArgsDataClass):
 
     @classmethod
     def get_effect_class(cls):
-        return BlackholeEffect
+        return Blackhole
 
 
-class BlackholeEffect(BaseEffect):
-    """Effect that creates a blackhole in a starfield, consumes the stars, and explodes the input characters out to position."""
-
-    def __init__(
-        self,
-        input_data: str,
-        effect_config: EffectConfig = EffectConfig(),
-        terminal_config: TerminalConfig = TerminalConfig(),
-    ):
-        """Initializes the BlackholeEffect.
-
-        Args:
-            input_data (str): The input data to be displayed.
-            effect_config (EffectConfig, optional): Configuration for the effect. Defaults to EffectConfig().
-            terminal_config (TerminalConfig, optional): Configuration for the terminal. Defaults to TerminalConfig().
-        """
-        self.terminal = Terminal(input_data, terminal_config)
-        self.config = effect_config
-        self._built = False
+class BlackholeIterator(BaseEffectIterator[BlackholeConfig]):
+    def __init__(self, effect: "Blackhole") -> None:
+        super().__init__(effect)
         self._pending_chars: list[EffectCharacter] = []
         self._active_chars: list[EffectCharacter] = []
         self._blackhole_chars: list[EffectCharacter] = []
         self._awaiting_consumption_chars: list[EffectCharacter] = []
         self._blackhole_radius = max(
             min(
-                round(self.terminal.output_area.right * 0.3),
-                round(self.terminal.output_area.top * 0.3),
+                round(self._terminal.output_area.right * 0.3),
+                round(self._terminal.output_area.top * 0.3),
             ),
             3,
         )
         self._character_final_color_map: dict[EffectCharacter, graphics.Color] = {}
+        self._build()
 
-    def prepare_blackhole(self) -> None:
+    def _prepare_blackhole(self) -> None:
         star_symbols = [
             "*",
             "✸",
@@ -148,11 +131,11 @@ class BlackholeEffect(BaseEffect):
         gradient_map = {}
         for color in starfield_colors:
             gradient_map[color] = graphics.Gradient(color, "000000", steps=10)
-        available_chars = list(self.terminal._input_characters)
+        available_chars = list(self._terminal._input_characters)
         while len(self._blackhole_chars) < self._blackhole_radius * 3 and available_chars:
             self._blackhole_chars.append(available_chars.pop(random.randrange(0, len(available_chars))))
         black_hole_ring_positions = geometry.find_coords_on_circle(
-            self.terminal.output_area.center,
+            self._terminal.output_area.center,
             self._blackhole_radius,
             len(self._blackhole_chars),
         )
@@ -161,7 +144,7 @@ class BlackholeEffect(BaseEffect):
             blackhole_path = character.motion.new_path(id="blackhole", speed=0.5, ease=easing.in_out_sine)
             blackhole_path.new_waypoint(starting_pos)
             blackhole_scn = character.animation.new_scene(id="blackhole")
-            blackhole_scn.add_frame("✸", 1, color=self.config.blackhole_color)
+            blackhole_scn.add_frame("✸", 1, color=self._config.blackhole_color)
             character.event_handler.register_event(
                 EventHandler.Event.PATH_ACTIVATED,
                 blackhole_path,
@@ -172,39 +155,39 @@ class BlackholeEffect(BaseEffect):
             blackhole_rotation_path = character.motion.new_path(id="blackhole_rotation", speed=0.2, loop=True)
             for coord in black_hole_ring_positions[position_index:] + black_hole_ring_positions[:position_index]:
                 blackhole_rotation_path.new_waypoint(coord, id=str(len(blackhole_rotation_path.waypoints)))
-        for character in self.terminal.get_characters():
-            self.terminal.set_character_visibility(character, True)
+        for character in self._terminal.get_characters():
+            self._terminal.set_character_visibility(character, True)
             starting_scn = character.animation.new_scene()
             star_symbol = random.choice(star_symbols)
             star_color = random.choice(starfield_colors)
             starting_scn.add_frame(star_symbol, 1, color=star_color)
             character.animation.activate_scene(starting_scn)
             if character not in self._blackhole_chars:
-                starfield_coord = self.terminal.output_area.random_coord()
+                starfield_coord = self._terminal.output_area.random_coord()
                 character.motion.set_coordinate(starfield_coord)
-                if starfield_coord.row > self.terminal.output_area.center_row:
+                if starfield_coord.row > self._terminal.output_area.center_row:
                     if starfield_coord.column in range(
-                        round(self.terminal.output_area.right * 0.4),
-                        round(self.terminal.output_area.right * 0.7),
+                        round(self._terminal.output_area.right * 0.4),
+                        round(self._terminal.output_area.right * 0.7),
                     ):
                         # if within the top center 40% of the screen
-                        control_point = Coord(self.terminal.output_area.center.column, starfield_coord.row)
+                        control_point = Coord(self._terminal.output_area.center.column, starfield_coord.row)
                     else:
-                        control_point = Coord(starfield_coord.column, self.terminal.output_area.center_row)
+                        control_point = Coord(starfield_coord.column, self._terminal.output_area.center_row)
 
-                elif starfield_coord.row < self.terminal.output_area.center_row:
+                elif starfield_coord.row < self._terminal.output_area.center_row:
                     if starfield_coord.column in range(
-                        round(self.terminal.output_area.right * 0.4),
-                        round(self.terminal.output_area.right * 0.7),
+                        round(self._terminal.output_area.right * 0.4),
+                        round(self._terminal.output_area.right * 0.7),
                     ):
                         # if within the bottom center 40% of the screen
-                        control_point = Coord(self.terminal.output_area.center.column, starfield_coord.row)
+                        control_point = Coord(self._terminal.output_area.center.column, starfield_coord.row)
                     else:
-                        control_point = Coord(starfield_coord.column, self.terminal.output_area.center_row)
+                        control_point = Coord(starfield_coord.column, self._terminal.output_area.center_row)
                 else:
-                    control_point = self.terminal.output_area.center
+                    control_point = self._terminal.output_area.center
                 singularity_path = character.motion.new_path(id="singularity", speed=0.3, ease=easing.in_expo)
-                singularity_path.new_waypoint(self.terminal.output_area.center, bezier_control=control_point)
+                singularity_path.new_waypoint(self._terminal.output_area.center, bezier_control=control_point)
                 consumed_scn = character.animation.new_scene()
                 for color in gradient_map[star_color]:
                     consumed_scn.add_frame(star_symbol, 1, color=color)
@@ -225,14 +208,14 @@ class BlackholeEffect(BaseEffect):
                 self._awaiting_consumption_chars.append(character)
         random.shuffle(self._awaiting_consumption_chars)
 
-    def rotate_blackhole(self) -> None:
+    def _rotate_blackhole(self) -> None:
         for character in self._blackhole_chars:
             character.motion.activate_path(character.motion.query_path("blackhole_rotation"))
             self._active_chars.append(character)
 
-    def collapse_blackhole(self) -> None:
+    def _collapse_blackhole(self) -> None:
         black_hole_ring_positions = geometry.find_coords_on_circle(
-            self.terminal.output_area.center,
+            self._terminal.output_area.center,
             self._blackhole_radius + 3,
             len(self._blackhole_chars),
         )
@@ -243,7 +226,7 @@ class BlackholeEffect(BaseEffect):
             expand_path = character.motion.new_path(speed=0.1, ease=easing.in_expo)
             expand_path.new_waypoint(next_pos)
             collapse_path = character.motion.new_path(speed=0.3, ease=easing.in_expo)
-            collapse_path.new_waypoint(self.terminal.output_area.center)
+            collapse_path.new_waypoint(self._terminal.output_area.center)
             character.event_handler.register_event(
                 EventHandler.Event.PATH_COMPLETE,
                 expand_path,
@@ -254,7 +237,7 @@ class BlackholeEffect(BaseEffect):
                 point_scn = character.animation.new_scene()
                 for _ in range(3):
                     for symbol in unstable_symbols:
-                        point_scn.add_frame(symbol, 6, color=random.choice(self.config.star_colors))
+                        point_scn.add_frame(symbol, 6, color=random.choice(self._config.star_colors))
                 character.event_handler.register_event(
                     EventHandler.Event.PATH_COMPLETE,
                     collapse_path,
@@ -272,9 +255,9 @@ class BlackholeEffect(BaseEffect):
             character.motion.activate_path(expand_path)
             self._active_chars.append(character)
 
-    def explode_singularity(self) -> None:
+    def _explode_singularity(self) -> None:
         star_colors = ["ffcc0d", "ff7326", "ff194d", "bf2669", "702a8c" "049dbf"]
-        for character in self.terminal.get_characters():
+        for character in self._terminal.get_characters():
             nearby_coord = geometry.find_coords_on_circle(character.input_coord, 3, 5)[random.randrange(0, 5)]
             nearby_path = character.motion.new_path(speed=random.randint(2, 3) / 10, ease=easing.out_expo)
             nearby_path.new_waypoint(nearby_coord)
@@ -304,90 +287,87 @@ class BlackholeEffect(BaseEffect):
             character.motion.activate_path(nearby_path)
             self._active_chars.append(character)
 
-    def build(self) -> None:
+    def _build(self) -> None:
         """Prepares the data for the effect by creating the starfield, blackhole, and consumption scenes/waypoints."""
-        self._pending_chars.clear()
-        self._active_chars.clear()
-        self._blackhole_chars.clear()
-        self._awaiting_consumption_chars.clear()
-        self._character_final_color_map.clear()
-        final_gradient = graphics.Gradient(*self.config.final_gradient_stops, steps=self.config.final_gradient_steps)
+        final_gradient = graphics.Gradient(*self._config.final_gradient_stops, steps=self._config.final_gradient_steps)
         final_gradient_mapping = final_gradient.build_coordinate_color_mapping(
-            self.terminal.output_area.top, self.terminal.output_area.right, self.config.final_gradient_direction
+            self._terminal.output_area.top, self._terminal.output_area.right, self._config.final_gradient_direction
         )
-        for character in self.terminal.get_characters():
+        for character in self._terminal.get_characters():
             self._character_final_color_map[character] = final_gradient_mapping[character.input_coord]
-        self.prepare_blackhole()
+        self._prepare_blackhole()
+        self.formation_delay = max(100 // len(self._blackhole_chars), 10)
+        self.f_delay = self.formation_delay
+        self.next_char_consuming_delay = 0
+        self.max_consume = max(min(int(0.1 * len(self._terminal._input_characters)), 15), 2)
+        self.phase = "forming"
+        self.awaiting_blackhole_chars = list(self._blackhole_chars)
 
-    @property
-    def built(self) -> bool:
-        """Returns True if the effect has been built."""
-        return self._built
-
-    def __iter__(self) -> Iterator[str]:
+    def __next__(self) -> str:
         """Runs the effect."""
-        if not self._built:
-            self.build()
-        formation_delay = max(100 // len(self._blackhole_chars), 10)
-        f_delay = formation_delay
-        next_char_consuming_delay = 0
-        max_consume = max(min(int(0.1 * len(self.terminal._input_characters)), 15), 2)
-        phase = "forming"
-        awaiting_blackhole_chars = list(self._blackhole_chars)
-        while self._active_chars or phase != "complete":
-            if phase == "forming":
-                if awaiting_blackhole_chars:
-                    if not f_delay:
-                        next_char = awaiting_blackhole_chars.pop(0)
+        if self._active_chars or self.phase != "complete":
+            if self.phase == "forming":
+                if self.awaiting_blackhole_chars:
+                    if not self.f_delay:
+                        next_char = self.awaiting_blackhole_chars.pop(0)
                         next_char.motion.activate_path(next_char.motion.query_path("blackhole"))
                         next_char.animation.activate_scene(next_char.animation.query_scene("blackhole"))
                         self._active_chars.append(next_char)
-                        f_delay = formation_delay
+                        self.f_delay = self.formation_delay
                     else:
-                        f_delay -= 1
+                        self.f_delay -= 1
                 else:
                     if not self._active_chars:
-                        self.rotate_blackhole()
-                        phase = "consuming"
-            elif phase == "consuming":
+                        self._rotate_blackhole()
+                        self.phase = "consuming"
+            elif self.phase == "consuming":
                 if self._awaiting_consumption_chars:
-                    if not next_char_consuming_delay:
-                        for _ in range(random.randrange(1, max_consume)):
+                    if not self.next_char_consuming_delay:
+                        for _ in range(random.randrange(1, self.max_consume)):
                             if self._awaiting_consumption_chars:
                                 next_char = self._awaiting_consumption_chars.pop(0)
                                 next_char.motion.activate_path(next_char.motion.query_path("singularity"))
                                 self._active_chars.append(next_char)
                             else:
                                 break
-                        max_consume += 1
-                        next_char_consuming_delay = random.randrange(0, 10)
+                        self.max_consume += 1
+                        self.next_char_consuming_delay = random.randrange(0, 10)
                     else:
-                        next_char_consuming_delay -= 1
+                        self.next_char_consuming_delay -= 1
 
                 else:
                     if all(character in self._blackhole_chars for character in self._active_chars):
-                        phase = "collapsing"
+                        self.phase = "collapsing"
 
-            elif phase == "collapsing":
-                self.collapse_blackhole()
-                phase = "exploding"
-            elif phase == "exploding":
+            elif self.phase == "collapsing":
+                self._collapse_blackhole()
+                self.phase = "exploding"
+            elif self.phase == "exploding":
                 if all(
                     [
                         character.motion.active_path is None and character.animation.active_scene is None
                         for character in self._blackhole_chars
                     ]
                 ):
-                    self.explode_singularity()
-                    phase = "complete"
+                    self._explode_singularity()
+                    self.phase = "complete"
 
-            self._animate_chars()
-            yield self.terminal.get_formatted_output_string()
+            for character in self._active_chars:
+                character.tick()
+            next_frame = self._terminal.get_formatted_output_string()
 
             self._active_chars = [character for character in self._active_chars if character.is_active]
-        self._built = False
+            return next_frame
 
-    def _animate_chars(self) -> None:
-        """Animates the characters by calling the tick method."""
-        for character in self._active_chars:
-            character.tick()
+        else:
+            raise StopIteration
+
+
+class Blackhole(BaseEffect[BlackholeConfig]):
+    """Effect that creates a blackhole in a starfield, consumes the stars, and explodes the input characters out to position."""
+
+    _config_cls = BlackholeConfig
+    _iterator_cls = BlackholeIterator
+
+    def __init__(self, input_data: str) -> None:
+        super().__init__(input_data)
