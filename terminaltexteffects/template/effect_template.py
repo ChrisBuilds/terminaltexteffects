3b235e4da8dc5fae7a991b109c8871162132a5fe
Chris <741258+github@pm.me>
2024-04-30 06:25:21 -0500

    Update color argument names in effect_template.py

--- terminaltexteffects/template/effect_template.py
+++ terminaltexteffects/template/effect_template.py
@@ -19,35 +19,35 @@ from terminaltexteffects.utils.argsdataclass import ArgField, ArgsDataClass, arg
 class EffectConfig(ArgsDataClass):
     color_single: graphics.Color = ArgField(
         cmd_name=["--color-single"],
-        type_parser=argvalidators.Color.type_parser,
+        type_parser=argvalidators.ColorArg.type_parser,
         default=0,
-        metavar=argvalidators.Color.METAVAR,
+        metavar=argvalidators.ColorArg.METAVAR,
         help="Color for the ___.",
     )  # type: ignore[assignment]
 
     color_list: tuple[graphics.Color, ...] = ArgField(
         cmd_name=["--color-list"],
-        type_parser=argvalidators.Color.type_parser,
+        type_parser=argvalidators.ColorArg.type_parser,
         nargs="+",
         default=0,
-        metavar=argvalidators.Color.METAVAR,
+        metavar=argvalidators.ColorArg.METAVAR,
         help="Space separated, unquoted, list of colors for the ___.",
     )  # type: ignore[assignment]
 
     final_color: graphics.Color = ArgField(
         cmd_name=["--final-color"],
-        type_parser=argvalidators.Color.type_parser,
+        type_parser=argvalidators.ColorArg.type_parser,
         default="ffffff",
-        metavar=argvalidators.Color.METAVAR,
+        metavar=argvalidators.ColorArg.METAVAR,
         help="Color for the final character.",
     )  # type: ignore[assignment]
 
     final_gradient_stops: tuple[graphics.Color, ...] = ArgField(
         cmd_name=["--final-gradient-stops"],
-        type_parser=argvalidators.Color.type_parser,
+        type_parser=argvalidators.ColorArg.type_parser,
         nargs="+",
         default=("8A008A", "00D1FF", "FFFFFF"),
-        metavar=argvalidators.Color.METAVAR,
+        metavar=argvalidators.ColorArg.METAVAR,
         help="Space separated, unquoted, list of colors for the character gradient (applied from bottom to top). If only one color is provided, the characters will be displayed in that color.",
     )  # type: ignore[assignment]
 
