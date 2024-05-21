9c710673dc07e2c3c05706f474895671b9130e18
Chris <741258+github@pm.me>
2024-05-21 06:25:00 -0500

    Adds support for slice-direction.

--- terminaltexteffects/effects/effect_verticalslice.py
+++ terminaltexteffects/effects/effect_verticalslice.py
@@ -1,9 +1,9 @@
-"""Slices the input in half vertically and slides it into place from opposite directions.
+"""Slices the input in half and slides it into place from opposite directions.
 
 Classes:
-    VerticalSlice: Slices the input in half vertically and slides it into place from opposite directions.
-    VerticalSliceConfig: Configuration for the VerticalSlice effect.
-    VerticalSliceIterator: Effect iterator for the effect. Does not normally need to be called directly.
+    Slice: Slices the input in half and slides it into place from opposite directions.
+    SliceConfig: Configuration for the Slice effect.
+    SliceIterator: Effect iterator for the effect. Does not normally need to be called directly.
 """
 
 import typing
@@ -18,25 +18,27 @@ from terminaltexteffects.utils.graphics import Color, Gradient
 
 
 def get_effect_and_args() -> tuple[type[typing.Any], type[ArgsDataClass]]:
-    return VerticalSlice, VerticalSliceConfig
+    return Slice, SliceConfig
 
 
 @argclass(
-    name="verticalslice",
-    help="Slices the input in half vertically and slides it into place from opposite directions.",
-    description="verticalslice | Slices the input in half vertically and slides it into place from opposite directions.",
+    name="slice",
+    help="Slices the input in half and slides it into place from opposite directions.",
+    description="slice | Slices the input in half and slides it into place from opposite directions.",
     epilog=f"""{argvalidators.EASING_EPILOG}
     
-Example: terminaltexteffects verticalslice --final-gradient-stops 8A008A 00D1FF FFFFFF --final-gradient-steps 12 --movement-speed 0.15 --movement-easing IN_OUT_EXPO""",
+Example: terminaltexteffects slice --final-gradient-stops 8A008A 00D1FF FFFFFF --final-gradient-steps 12
+--slice-direction vertical--movement-speed 0.15 --movement-easing IN_OUT_EXPO""",
 )
 @dataclass
-class VerticalSliceConfig(ArgsDataClass):
-    """Configuration for the VerticalSlice effect.
+class SliceConfig(ArgsDataClass):
+    """Configuration for the Slice effect.
 
     Attributes:
         final_gradient_stops (tuple[Color, ...]): Tuple of colors for the final color gradient. If only one color is provided, the characters will be displayed in that color.
         final_gradient_steps (tuple[int, ...] | int): Tuple of the number of gradient steps to use. More steps will create a smoother and longer gradient animation. Valid values are n > 0.
         final_gradient_direction (Gradient.Direction): Direction of the final gradient.
+        slice_direction (typing.Literal["vertical", "horizontal", "diagonal"]): Direction of the slice.
         movement_speed (float): Movement speed of the characters. Valid values are n > 0.
         movement_easing (easing.EasingFunction): Easing function to use for character movement.
     """
@@ -64,11 +66,19 @@ class VerticalSliceConfig(ArgsDataClass):
     final_gradient_direction: Gradient.Direction = ArgField(
         cmd_name="--final-gradient-direction",
         type_parser=argvalidators.GradientDirection.type_parser,
-        default=Gradient.Direction.VERTICAL,
+        default=Gradient.Direction.DIAGONAL,
         metavar=argvalidators.GradientDirection.METAVAR,
         help="Direction of the final gradient.",
     )  # type: ignore[assignment]
     "Gradient.Direction : Direction of the final gradient."
+
+    slice_direction: typing.Literal["vertical", "horizontal", "diagonal"] = ArgField(
+        cmd_name="--slice-direction",
+        default="vertical",
+        choices=["vertical", "horizontal", "diagonal"],
+        help="Direction of the slice.",
+    )  # type: ignore[assignment]
+    "typing.Literal['vertical', 'horizontal', 'diagonal'] : Direction of the slice."
 
     movement_speed: float = ArgField(
         cmd_name="--movement-speed",
@@ -77,7 +87,7 @@ class VerticalSliceConfig(ArgsDataClass):
         metavar=argvalidators.PositiveFloat.METAVAR,
         help="Movement speed of the characters. ",
     )  # type: ignore[assignment]
-    "float : Movement speed of the characters. "
+    "float : Movement speed of the characters. Doubled for horizontal slices."
 
     movement_easing: easing.EasingFunction = ArgField(
         cmd_name="--movement-easing",
@@ -89,18 +99,22 @@ class VerticalSliceConfig(ArgsDataClass):
 
     @classmethod
     def get_effect_class(cls):
-        return VerticalSlice
+        return Slice
 
 
-class VerticalSliceIterator(BaseEffectIterator[VerticalSliceConfig]):
-    def __init__(self, effect: "VerticalSlice") -> None:
+class SliceIterator(BaseEffectIterator[SliceConfig]):
+    def __init__(self, effect: "Slice") -> None:
         super().__init__(effect)
-        self.pending_chars: list[EffectCharacter] = []
-        self.new_rows: list[list[EffectCharacter]] = []
+        self.pending_groups: list[list[EffectCharacter]] = []
         self.character_final_color_map: dict[EffectCharacter, Color] = {}
         self.build()
 
     def build(self) -> None:
+        slice_direction_map = {
+            "vertical": self.terminal.CharacterGroup.ROW_BOTTOM_TO_TOP,
+            "horizontal": self.terminal.CharacterGroup.COLUMN_RIGHT_TO_LEFT,
+            "diagonal": self.terminal.CharacterGroup.DIAGONAL_BOTTOM_LEFT_TO_TOP_RIGHT,
+        }
         final_gradient = Gradient(*self.config.final_gradient_stops, steps=self.config.final_gradient_steps)
         final_gradient_mapping = final_gradient.build_coordinate_color_mapping(
             self.terminal.canvas.top, self.terminal.canvas.right, self.config.final_gradient_direction
@@ -111,56 +125,117 @@ class VerticalSliceIterator(BaseEffectIterator[VerticalSliceConfig]):
                 character.input_symbol,
                 self.character_final_color_map[character],
             )
+        if self.config.slice_direction == "vertical":
+            self.rows = self.terminal.get_characters_grouped(grouping=slice_direction_map[self.config.slice_direction])
+            lengths = [max([c.input_coord.column for c in row]) for row in self.rows]
+            mid_point = sum(lengths) // len(lengths) // 2
+            for row_index, row in enumerate(self.rows):
+                new_row = []
 
-        self.rows = self.terminal.get_characters_grouped(grouping=self.terminal.CharacterGroup.ROW_BOTTOM_TO_TOP)
-        lengths = [max([c.input_coord.column for c in row]) for row in self.rows]
-        mid_point = sum(lengths) // len(lengths) // 2
-        for row_index, row in enumerate(self.rows):
-            new_row = []
-            left_half = [character for character in row if character.input_coord.column <= mid_point]
-            for character in left_half:
-                character.motion.set_coordinate(Coord(character.input_coord.column, self.terminal.canvas.top))
-                input_coord_path = character.motion.new_path(
-                    speed=self.config.movement_speed, ease=self.config.movement_easing
-                )
-                input_coord_path.new_waypoint(character.input_coord)
-                character.motion.activate_path(input_coord_path)
-            opposite_row = self.rows[-(row_index + 1)]
-            right_half = [c for c in opposite_row if c.input_coord.column > mid_point]
-            for character in right_half:
-                character.motion.set_coordinate(Coord(character.input_coord.column, self.terminal.canvas.bottom))
-                input_coord_path = character.motion.new_path(
-                    speed=self.config.movement_speed, ease=self.config.movement_easing
-                )
-                input_coord_path.new_waypoint(character.input_coord)
-                character.motion.activate_path(input_coord_path)
-            new_row.extend(left_half)
-            new_row.extend(right_half)
-            self.new_rows.append(new_row)
+                left_half = [character for character in row if character.input_coord.column <= mid_point]
+                for character in left_half:
+                    character.motion.set_coordinate(Coord(character.input_coord.column, self.terminal.canvas.top + 1))
+                    input_coord_path = character.motion.new_path(
+                        speed=self.config.movement_speed, ease=self.config.movement_easing
+                    )
+                    input_coord_path.new_waypoint(character.input_coord)
+                    character.motion.activate_path(input_coord_path)
+                opposite_row = self.rows[-(row_index + 1)]
+                right_half = [c for c in opposite_row if c.input_coord.column > mid_point]
+                for character in right_half:
+                    character.motion.set_coordinate(
+                        Coord(character.input_coord.column, self.terminal.canvas.bottom - 1)
+                    )
+                    input_coord_path = character.motion.new_path(
+                        speed=self.config.movement_speed, ease=self.config.movement_easing
+                    )
+                    input_coord_path.new_waypoint(character.input_coord)
+                    character.motion.activate_path(input_coord_path)
+                new_row.extend(left_half)
+                new_row.extend(right_half)
+                self.active_characters.extend(new_row)
+        elif self.config.slice_direction == "horizontal":
+            self.config.movement_speed *= 2
+            self.columns = self.terminal.get_characters_grouped(
+                grouping=slice_direction_map[self.config.slice_direction], fill_chars=True
+            )
+            lengths = [max([c.input_coord.row for c in column]) for column in self.columns]
+            mid_point = sum(lengths) // len(lengths) // 2
+            for column_index, column in enumerate(self.columns):
+                new_column = []
+
+                bottom_half = [character for character in column if character.input_coord.row <= mid_point]
+                for character in bottom_half:
+                    character.motion.set_coordinate(Coord(self.terminal.canvas.left - 1, character.input_coord.row))
+                    input_coord_path = character.motion.new_path(
+                        speed=self.config.movement_speed, ease=self.config.movement_easing
+                    )
+                    input_coord_path.new_waypoint(character.input_coord)
+                    character.motion.activate_path(input_coord_path)
+                opposite_column = self.columns[-(column_index + 1)]
+                top_half = [c for c in opposite_column if c.input_coord.row > mid_point]
+                for character in top_half:
+                    character.motion.set_coordinate(Coord(self.terminal.canvas.right + 1, character.input_coord.row))
+                    input_coord_path = character.motion.new_path(
+                        speed=self.config.movement_speed, ease=self.config.movement_easing
+                    )
+                    input_coord_path.new_waypoint(character.input_coord)
+                    character.motion.activate_path(input_coord_path)
+                new_column.extend(bottom_half)
+                new_column.extend(top_half)
+                self.active_characters.extend(new_column)
+        elif self.config.slice_direction == "diagonal":
+            self.diagonals = self.terminal.get_characters_grouped(
+                grouping=slice_direction_map[self.config.slice_direction]
+            )
+            left = self.diagonals[: len(self.diagonals) // 2]
+            right = self.diagonals[len(self.diagonals) // 2 :]
+            while left or right:
+                new_group = []
+                if left:
+                    left_group = left.pop(0)
+                    origin_coord = Coord(left_group[0].input_coord.column, self.terminal.canvas.bottom - 1)
+                    for character in left_group:
+                        character.motion.set_coordinate(origin_coord)
+                        input_coord_path = character.motion.new_path(
+                            speed=self.config.movement_speed, ease=self.config.movement_easing
+                        )
+                        input_coord_path.new_waypoint(character.input_coord)
+                        character.motion.activate_path(input_coord_path)
+                    new_group.extend(left_group)
+                if right:
+                    right_group = right.pop(0)
+                    origin_coord = Coord(right_group[-1].input_coord.column, self.terminal.canvas.top + 1)
+                    for character in right_group:
+                        character.motion.set_coordinate(origin_coord)
+                        input_coord_path = character.motion.new_path(
+                            speed=self.config.movement_speed, ease=self.config.movement_easing
+                        )
+                        input_coord_path.new_waypoint(character.input_coord)
+                        character.motion.activate_path(input_coord_path)
+                    new_group.extend(right_group)
+                self.active_characters.extend(new_group)
+        for character in self.active_characters:
+            self.terminal.set_character_visibility(character, True)
 
     def __next__(self) -> str:
-        if self.new_rows or self.active_characters:
-            if self.new_rows:
-                next_row = self.new_rows.pop(0)
-                for character in next_row:
-                    self.terminal.set_character_visibility(character, True)
-                self.active_characters.extend(next_row)
+        if self.active_characters:
             self.update()
             return self.frame
         else:
             raise StopIteration
 
 
-class VerticalSlice(BaseEffect[VerticalSliceConfig]):
-    """Slices the input in half vertically and slides it into place from opposite directions.
+class Slice(BaseEffect[SliceConfig]):
+    """Slices the input in half and slides it into place from opposite directions.
 
     Attributes:
-        effect_config (VerticalSliceConfig): Configuration for the effect.
+        effect_config (SliceConfig): Configuration for the effect.
         terminal_config (TerminalConfig): Configuration for the terminal.
     """
 
-    _config_cls = VerticalSliceConfig
-    _iterator_cls = VerticalSliceIterator
+    _config_cls = SliceConfig
+    _iterator_cls = SliceIterator
 
     def __init__(self, input_data: str) -> None:
         """Initialize the effect with the provided input data.
