75b8ab781d6171b1b2aee5758f45d2a9d0cbef96
Chris <741258+github@pm.me>
2024-06-20 06:01:25 -0500

    untrack

--- terminaltexteffects/lib_tests/testing.py
+++
@@ -1,7 +0,0 @@
-from terminaltexteffects.effects.effect_matrix import Matrix
-
-effect = Matrix("YourTextHere\n" * 10)
-effect.effect_config.rain_time = 0
-with effect.terminal_output() as terminal:
-    for frame in effect:
-        terminal.print(frame)
