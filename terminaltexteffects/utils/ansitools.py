7765867c8a6a18295d0fb5a0a11e86099a9b68d4
Chris <741258+github@pm.me>
2024-07-04 07:52:45 -0500

    Adds parse_ansi_color_sequence

--- terminaltexteffects/utils/ansitools.py
+++ terminaltexteffects/utils/ansitools.py
@@ -2,6 +2,39 @@
 These escape codes can be used to modify the appearance of text in a terminal.
 """
 
+from __future__ import annotations
+
+import re
+
+
+def parse_ansi_color_sequence(sequence: str) -> int | str:
+    """Parses an 8-bit or 24-bit ANSI color sequence and returns the color code as an integer, in the case of 8-bit, or
+    a hex string in the case of 24-bit.
+
+    Args:
+        sequence (str): ANSI color sequence
+
+    Returns:
+        int | str: 8-bit color int or 24-bit color str
+    """
+    # Remove escape characters
+    sequence = re.sub(r"(\033\[|\x1b\[)", "", sequence).strip("m")
+    # detect 24-bit colors
+    if re.match(r"^(38;2|48;2)", sequence):
+        sequence = re.sub(r"^(38;2;|48;2;)", "", sequence)
+        colors = []
+        for color in sequence.split(";"):
+            if color:
+                colors.append(int(color))
+            else:
+                colors.append(0)  # default to 0 if no value in field (e.g. 38;2;;0m)
+        return "".join(f"{color:02X}" for color in colors)
+    # detect 8-bit colors
+    elif re.match(r"^(38;5|48;5)", sequence):
+        sequence = re.sub(r"^(38;5;|48;5;)", "", sequence)
+        return int(sequence)
+    raise ValueError("Invalid ANSI color sequence")
+
 
 def DEC_SAVE_CURSOR_POSITION() -> str:
     """Saves the cursor position using DEC sequence.
