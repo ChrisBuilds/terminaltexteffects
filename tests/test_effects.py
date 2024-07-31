d3c8c1a92211912e780cd97e3d27f2d38f89aa8b
Chris <741258+github@pm.me>
2024-07-31 11:56:01 -0500

    adds visual tests

--- tests/test_effects.py
+++ tests/test_effects.py
@@ -6,13 +6,24 @@ from terminaltexteffects.effects import effect_matrix
 @pytest.mark.parametrize(
     "input_data", ["empty", "single_char", "single_column", "single_row", "medium", "tabs"], indirect=True
 )
-def test_effect(effect, input_data, terminal_config_default) -> None:
+def test_effect(effect, input_data, terminal_config_default_no_framerate) -> None:
     effect = effect(input_data)
     # customize some effect configs to shorten testing time
     if isinstance(effect, effect_matrix.Matrix):
         effect.effect_config.rain_time = 1
-    effect.terminal_config = terminal_config_default
+    effect.terminal_config = terminal_config_default_no_framerate
 
+    with effect.terminal_output() as terminal:
+        for frame in effect:
+            terminal.print(frame)
+
+
+@pytest.mark.visual
+@pytest.mark.parametrize("input_data", ["large"], indirect=True)
+def test_effect_visual(effect, input_data) -> None:
+    effect = effect(input_data)
+    if isinstance(effect, effect_matrix.Matrix):
+        effect.effect_config.rain_time = 5
     with effect.terminal_output() as terminal:
         for frame in effect:
             terminal.print(frame)
