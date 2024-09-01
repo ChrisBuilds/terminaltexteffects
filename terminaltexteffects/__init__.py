f553d9fae479ed9d5aabeae94c22b07f05a82849
Chris <741258+github@pm.me>
2024-09-01 12:36:06 -0500

    Import class and modules to enable importing from the package.

--- terminaltexteffects/__init__.py
+++ terminaltexteffects/__init__.py
@@ -1,1 +1,9 @@
+from terminaltexteffects.engine.animation import Animation, Scene
+from terminaltexteffects.engine.base_character import EffectCharacter, EventHandler
+from terminaltexteffects.engine.motion import Motion, Path, Segment, Waypoint
+from terminaltexteffects.engine.terminal import Terminal
+from terminaltexteffects.utils import easing, geometry
+from terminaltexteffects.utils.geometry import Coord
+from terminaltexteffects.utils.graphics import Color, Gradient
+
 __version__ = "0.11.0"
