6acf016d21a7c206e143ca499f982b62b8b0611a
Chris <741258+github@pm.me>
2024-09-01 12:37:28 -0500

    ruff ignore

--- terminaltexteffects/__init__.py
+++ terminaltexteffects/__init__.py
@@ -1,9 +1,9 @@
-from terminaltexteffects.engine.animation import Animation, Scene
-from terminaltexteffects.engine.base_character import EffectCharacter, EventHandler
-from terminaltexteffects.engine.motion import Motion, Path, Segment, Waypoint
-from terminaltexteffects.engine.terminal import Terminal
-from terminaltexteffects.utils import easing, geometry
-from terminaltexteffects.utils.geometry import Coord
-from terminaltexteffects.utils.graphics import Color, Gradient
+from terminaltexteffects.engine.animation import Animation, Scene  # noqa: F401
+from terminaltexteffects.engine.base_character import EffectCharacter, EventHandler  # noqa: F401
+from terminaltexteffects.engine.motion import Motion, Path, Segment, Waypoint  # noqa: F401
+from terminaltexteffects.engine.terminal import Terminal  # noqa: F401
+from terminaltexteffects.utils import easing, geometry  # noqa: F401
+from terminaltexteffects.utils.geometry import Coord  # noqa: F401
+from terminaltexteffects.utils.graphics import Color, Gradient  # noqa: F401
 
 __version__ = "0.11.0"
