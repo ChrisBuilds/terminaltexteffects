2a895a4b2fd18b16f5a9ca8b034b2898b09f5f89
Chris <741258+github@pm.me>
2024-11-30 13:05:21 -0500

    Linting

--- terminaltexteffects/__init__.py
+++ terminaltexteffects/__init__.py
@@ -1,9 +1,19 @@
-from terminaltexteffects.engine.animation import Animation, Scene  # noqa: F401
-from terminaltexteffects.engine.base_character import EffectCharacter, EventHandler  # noqa: F401
-from terminaltexteffects.engine.motion import Motion, Path, Segment, Waypoint  # noqa: F401
-from terminaltexteffects.engine.terminal import Terminal  # noqa: F401
-from terminaltexteffects.utils import easing, geometry  # noqa: F401
-from terminaltexteffects.utils.geometry import Coord  # noqa: F401
-from terminaltexteffects.utils.graphics import Color, ColorPair, Gradient  # noqa: F401
+"""Terminal Text Effects package.
+
+This package provides various text effects for terminal applications.
+"""
+
+from terminaltexteffects.engine.animation import Animation, Scene
+from terminaltexteffects.engine.base_character import EffectCharacter, EventHandler
+from terminaltexteffects.engine.motion import (
+    Motion,
+    Path,
+    Segment,
+    Waypoint,
+)
+from terminaltexteffects.engine.terminal import Terminal
+from terminaltexteffects.utils import easing, geometry
+from terminaltexteffects.utils.geometry import Coord
+from terminaltexteffects.utils.graphics import Color, ColorPair, Gradient
 
 __version__ = "0.12.0"
