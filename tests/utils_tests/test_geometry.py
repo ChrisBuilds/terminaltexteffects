34b0dd5d2e861433e0a815901f2b9846b86f1dd6
Chris <741258+github@pm.me>
2025-01-24 15:20:38 -0500

    linting

--- tests/utils_tests/test_geometry.py
+++ tests/utils_tests/test_geometry.py
@@ -87,7 +87,7 @@ def test_find_coord_on_bezier_curve_two_control_points():
 
 
 def test_find_coord_on_bezier_curve_invalid_t():
-    geometry.find_coord_at_distance.cache_clear()
+    geometry.find_coord_at_distance.cache_clear()  # type: ignore[reportFunctionMemberAccess]
     start = geometry.Coord(0, 0)
     end = geometry.Coord(10, 10)
     control = geometry.Coord(5, 0)
