092a84b28144e1e24b3efb022f7608ec3f2f01c7
ChrisBuilds <57874186+ChrisBuilds@users.noreply.github.com>
2025-08-15 12:42:25 +0000

    Build terminal and effect configs and pass them to the effect constructor.

--- terminaltexteffects/__main__.py
+++ terminaltexteffects/__main__.py
@@ -7,17 +7,19 @@ import importlib
 import pkgutil
 import sys
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import terminaltexteffects.effects
-import terminaltexteffects.engine.terminal as term
-from terminaltexteffects.engine.base_config import BaseConfig
-from terminaltexteffects.engine.base_effect import BaseEffect
-from terminaltexteffects.engine.terminal import TerminalConfig
+from terminaltexteffects.engine.terminal import Terminal, TerminalConfig
+
+if TYPE_CHECKING:
+    from terminaltexteffects.engine.base_config import BaseConfig
+    from terminaltexteffects.engine.base_effect import BaseEffect
 
 
 def main() -> None:
     """Run the terminaltexteffects command line interface."""
-    parser = (argparse.ArgumentParser)(
+    parser = argparse.ArgumentParser(
         prog="tte",
         description="A terminal visual effects engine, application, and library",
         epilog="Ex: ls -a | tte decrypt --typing-speed 2 --ciphertext-colors 008000 00cb00 00ff00 "
@@ -41,21 +43,20 @@ def main() -> None:
         required=True,
         dest="effect",
     )
-    effect_map: dict[str, tuple[type[BaseEffect], type[BaseConfig]]] = {}
+    effect_resource_map: dict[str, tuple[type[BaseEffect], type[BaseConfig]]] = {}
     for module_info in pkgutil.iter_modules(
         terminaltexteffects.effects.__path__,
         terminaltexteffects.effects.__name__ + ".",
     ):
         module = importlib.import_module(module_info.name)
 
-        if hasattr(module, "get_effect_and_config"):
-            effect_cmd, effect_class, args_class = module.get_effect_and_config()
-        else:
-            effect_class, args_class = module.get_effect_and_args()
-            effect_cmd = "unknown"
-        effect_map[effect_cmd] = (effect_class, args_class)
-        if "effect_burn" in str(effect_class):  # no commit
-            args_class._populate_parser(subparsers)
+        if hasattr(module, "get_effect_resources"):
+            effect_cmd: str
+            effect_class: type[BaseEffect]
+            config_class: type[BaseConfig]
+            effect_cmd, effect_class, config_class = module.get_effect_resources()
+            effect_resource_map[effect_cmd] = (effect_class, config_class)
+            config_class._populate_parser(subparsers)
 
     args = parser.parse_args()
     if args.input_file:
@@ -68,21 +69,21 @@ def main() -> None:
             print(f"Error reading file: {args.input_file} - {e}")
             return
     else:
-        input_data = term.Terminal.get_piped_input()
+        input_data = Terminal.get_piped_input()
     if not input_data.strip():
         print("NO INPUT.")
-    else:
-        terminal_config = TerminalConfig._build_config(args)
-        effect_class, effect_config = effect_map[args.effect]
-        effect = effect_class(input_data)
-        effect.effect_config = effect_config._build_config(args)
-        effect.terminal_config = terminal_config
-        try:
-            with effect.terminal_output() as terminal:
-                for frame in effect:
-                    terminal.print(frame)
-        except KeyboardInterrupt:
-            sys.exit(1)
+        return
+
+    effect_class, effect_config_class = effect_resource_map[args.effect]
+    terminal_config = TerminalConfig._build_config(args)
+    effect_config = effect_config_class._build_config(args)
+    effect = effect_class(input_data, effect_config, terminal_config)
+    try:
+        with effect.terminal_output() as terminal:
+            for frame in effect:
+                terminal.print(frame)
+    except KeyboardInterrupt:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
