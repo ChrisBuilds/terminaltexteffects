36ac2e7120d7dbd93af5f3c9b06c3b51f24c7c5b
Chris <741258+github@pm.me>
2025-09-16 15:51:37 -0400

    adjust speeds

--- terminaltexteffects/effects/effect_crumble.py
+++ terminaltexteffects/effects/effect_crumble.py
@@ -129,20 +129,20 @@ class CrumbleIterator(BaseEffectIterator[CrumbleConfig]):
             initial_scn.add_frame(character.input_symbol, 1, colors=ColorPair(fg=weak_color))
             character.animation.activate_scene(initial_scn)
             fall_path = character.motion.new_path(
-                speed=0.35,
+                speed=0.65,
                 ease=easing.out_bounce,
             )
             fall_path.new_waypoint(Coord(character.input_coord.column, self.terminal.canvas.bottom))
             weaken_scn = character.animation.new_scene(scene_id="weaken")
             weaken_scn.apply_gradient_to_symbols(character.input_symbol, 4, fg_gradient=weaken_gradient)
 
-            top_path = character.motion.new_path(path_id="top", speed=0.7, ease=easing.out_quint)
+            top_path = character.motion.new_path(path_id="top", speed=1, ease=easing.out_quint)
             top_path.new_waypoint(
                 Coord(character.input_coord.column, self.terminal.canvas.top),
                 bezier_control=Coord(self.terminal.canvas.center_column, self.terminal.canvas.center_row),
             )
             # set up reset stage
-            input_path = character.motion.new_path(path_id="input", speed=0.5)
+            input_path = character.motion.new_path(path_id="input", speed=1)
             input_path.new_waypoint(character.input_coord)
             strengthen_flash_scn = character.animation.new_scene()
             strengthen_flash_scn.apply_gradient_to_symbols(
