bc120ada82262644e433a51e92e84f7e85903b4c
Chris <741258+github@pm.me>
2025-11-28 08:47:03 -0500

    removes duplicate ArgSpec keyword arg

--- terminaltexteffects/template/effect_template.py
+++ terminaltexteffects/template/effect_template.py
@@ -69,7 +69,6 @@ class EffectConfig(BaseConfig):
         nargs="+",
         action=argutils.TupleAction,
         default=12,
-        action=argutils.TupleAction,
         metavar=argutils.PositiveInt.METAVAR,
         help=(
             "Space separated, unquoted, list of the number of gradient steps to use. More steps will "
