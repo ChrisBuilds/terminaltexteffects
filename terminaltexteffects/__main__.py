248471250a552dffd2dbd1d1a9cdc5e8a35c984b
Chris <741258+github@pm.me>
2025-11-28 00:02:47 -0500

    refactors random effect handling

--- terminaltexteffects/__main__.py
+++ terminaltexteffects/__main__.py
@@ -18,8 +18,14 @@ if TYPE_CHECKING:
     from terminaltexteffects.engine.base_effect import BaseEffect
 
 
-def main() -> None:
-    """Run the terminaltexteffects command line interface."""
+def build_parsers_and_parse_args() -> tuple[argparse.Namespace, dict[str, tuple[type[BaseEffect], type[BaseConfig]]]]:
+    """Build the argument parsers for the terminaltexteffects CLI and parse the arguments.
+
+    Returns:
+        tuple[argparse.Namespace, dict[str, tuple[type[BaseEffect], type[BaseConfig]]]]: The parsed arguments and a
+            mapping of effect names to their classes and configurations.
+
+    """
     parser = argparse.ArgumentParser(
         prog="tte",
         description="A terminal visual effects engine, application, and library",
@@ -34,6 +40,20 @@ def main() -> None:
         action="version",
         version="TerminalTextEffects " + terminaltexteffects.__version__,
     )
+    parser.add_argument("--random-effect", "-R", action="store_true", help="Randomly select an effect to apply")
+    random_include_exclude_group = parser.add_mutually_exclusive_group()
+    random_include_exclude_group.add_argument(
+        "--include-effects",
+        type=str,
+        nargs="+",
+        help="Space-separated list of Effects to include when randomly selecting an effect",
+    )
+    random_include_exclude_group.add_argument(
+        "--exclude-effects",
+        type=str,
+        nargs="+",
+        help="Space-separated list of Effects to exclude when randomly selecting an effect",
+    )
 
     TerminalConfig._populate_parser(parser)
 
@@ -41,16 +61,10 @@ def main() -> None:
         title="Effect",
         description="Name of the effect to apply. Use <effect> -h for effect specific help.",
         help="Available Effects",
-        required=True,
+        required=False,
         dest="effect",
     )
-    subparsers.add_parser(
-        name="random_effect",
-        help=(
-            "Randomly select an effect to apply to the input text. All effect and effect-specific options are ignored."
-        ),
-        description=("Random effect."),
-    )
+
     effect_resource_map: dict[str, tuple[type[BaseEffect], type[BaseConfig]]] = {}
     for module_info in pkgutil.iter_modules(
         terminaltexteffects.effects.__path__,
@@ -63,34 +77,52 @@ def main() -> None:
             effect_class: type[BaseEffect]
             config_class: type[BaseConfig]
             effect_cmd, effect_class, config_class = module.get_effect_resources()
+            if effect_cmd in effect_resource_map:
+                msg = f"Duplicate effect command detected: {effect_cmd}"
+                raise ValueError(msg)
             effect_resource_map[effect_cmd] = (effect_class, config_class)
             config_class._populate_parser(subparsers)
 
-    # check for random_effect subparser selection and replace
-    # with an effect name prior to parsing, otherwise default
-    # config options are not processed
-    if "random_effect" in sys.argv:
-        sys.argv[sys.argv.index("random_effect")] = random.choice(list(effect_resource_map.keys()))
+    return parser.parse_args(), effect_resource_map
 
-    args = parser.parse_args()
+
+def main() -> None:
+    """Run the terminaltexteffects command line interface."""
+    args, effect_resource_map = build_parsers_and_parse_args()
     if args.input_file:
         try:
             input_data = Path(args.input_file).read_text(encoding="UTF-8")
         except FileNotFoundError:
             print(f"File not found: {args.input_file}")
-            return
+            sys.exit(1)
         except Exception as e:  # noqa: BLE001
             print(f"Error reading file: {args.input_file} - {e}")
-            return
+            sys.exit(1)
     else:
         input_data = Terminal.get_piped_input()
     if not input_data.strip():
         print("NO INPUT.")
-        return
+        sys.exit(1)
+
+    if args.random_effect:
+        if args.include_effects:
+            available_effects = [effect for effect in effect_resource_map if effect in args.include_effects]
+        elif args.exclude_effects:
+            available_effects = [effect for effect in effect_resource_map if effect not in args.exclude_effects]
+        else:
+            available_effects = list(effect_resource_map)
+        if not available_effects:
+            print("Error: No effects available for random selection based on include/exclude filters.\n")
+            sys.exit(1)
+
+        args.effect = random.choice(available_effects)
+    elif not args.effect:
+        print("Error: No effect specified. Must specify an effect or use --random-effect.\n")
+        sys.exit(1)
 
     effect_class, effect_config_class = effect_resource_map[args.effect]
     terminal_config = TerminalConfig._build_config(args)
-    effect_config = effect_config_class._build_config(args)
+    effect_config = effect_config_class._build_config(None if args.random_effect else args)
     effect = effect_class(input_data, effect_config, terminal_config)
     try:
         with effect.terminal_output() as terminal:
